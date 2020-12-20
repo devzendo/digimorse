be28327a3255ad6e127c2cd2418012f482a58938
Matt Gumbley <matt.gumbley@devzendo.org>
2020-12-20 15:42:25 +0000

    Move state variables into the ArduinoKeyer.

--- src/libs/keyer_io/arduino_keyer_io.rs
+++ src/libs/keyer_io/arduino_keyer_io.rs
@@ -4,69 +4,41 @@ use crate::libs::keyer_io::keyer_io::{Keyer, KeyingEdgeEventListener, KeyerPolar
 use crate::libs::serial_io::serial_io::SerialIO;
 use std::io::Error;
 use crate::libs::util::util::printable;
+use crate::libs::keyer_io::arduino_keyer_io::KeyerState::Initial;
+
+pub enum KeyerState {
+    Initial,
+    KeyingDurationGetLSB, KeyingDurationGetMSB,
+    ResponseGotGt, ResponseGotSpc, ResponseFinish
+}
 
 pub struct ArduinoKeyer<'a> {
     serial_io:&'a mut (SerialIO + 'a),
     // thread waiting for data, or async read/promise
     // current callback to receive non-pulse/timing data (lines starting with > until blank NL)
+    state: KeyerState,
+    read_text: Vec<u8>,
+    read_buf: [u8; 1],
+    start_of_line: bool,
 }
 
 impl<'a> ArduinoKeyer<'a> {
     fn new(s: &'a mut SerialIO) -> Self {
         Self {
-            serial_io: s
+            serial_io: s,
+            state: Initial,
+            read_text: vec![],
+            read_buf: [0],
+            start_of_line: true
         }
     }
-/*    fn transact(&self, command: String) -> Result<String, String> {
-        // set callback to build up
-        // send(command);
-    }
-*/
 }
+
 impl<'a> Keyer for ArduinoKeyer<'a> {
     fn get_version(&mut self) -> Result<String, String> {
         let keyer_will_send = "v\n";
-        let written_bytes = self.serial_io.write(keyer_will_send.as_bytes());
-        match written_bytes {
-            Ok(n) => {
-                debug!("Written {} bytes to keyer", n);
-                let mut read_text: Vec<u8> = vec![];
-                let mut read_buf: [u8; 1] = [0];
-                let mut start_of_line: bool = true;
-                loop {
-                    let read_bytes = self.serial_io.read(&mut read_buf);
-                    match read_bytes {
-                        Ok(1) => {
-                            debug!("transact read {}", printable(read_buf[0]));
-                            if read_buf[0] == 0x0a {
-                                debug!("Got NL...");
-                                if start_of_line {
-                                    debug!("NL read on its own: end of response");
-                                    let mut subslice = &read_text[2..read_text.len() - 1];
 
-                                    return Ok(std::str::from_utf8(subslice).expect("Found invalid UTF-8").parse().unwrap());
-                                }
-                                start_of_line = true;
-                            } else {
-                                debug!("Got non-NL...");
-                                start_of_line = false;
-                            }
-                            read_text.push(read_buf[0]);
-                        }
-                        Ok(n) => {
-                            warn!("In build loop, received {} bytes", n);
-                            return Err(format!("Build loop stopped reading, received {} bytes", n));
-                        }
-                        Err(e) => {
-                            return Err(format!("Could not read data from keyer: {}", e.to_string()))
-                        }
-                    }
-                }
-            }
-            Err(e) => {
-                return Err(format!("Could not write command to keyer: {}", e.to_string()))
-            }
-        }
+        self.transact(keyer_will_send)
     }
 
     fn get_speed(&mut self) -> Result<u8, String> {
@@ -107,6 +79,51 @@ impl<'a> Keyer for ArduinoKeyer<'a> {
 
     fn clear_edge_event_listener(&mut self) {
         unimplemented!()
+    }
+}
+
+impl<'a> ArduinoKeyer<'a> {
+    fn transact(&mut self, command_to_keyer: &str) -> Result<String, String> {
+        let written_bytes = self.serial_io.write(command_to_keyer.as_bytes());
+        match written_bytes {
+            Ok(n) => {
+                debug!("Written {} bytes to keyer", n);
+                self.state = Initial;
+                loop {
+                    let read_bytes = self.serial_io.read(&mut self.read_buf);
+                    match read_bytes {
+                        Ok(1) => {
+                            debug!("transact read {}", printable(self.read_buf[0]));
+                            if self.read_buf[0] == 0x0a {
+                                debug!("Got NL...");
+                                if self.start_of_line {
+                                    debug!("NL read on its own: end of response");
+                                    // Given ">_XXXX\n" return "XXXX"
+                                    let mut subslice = &self.read_text[2..self.read_text.len() - 1];
+
+                                    return Ok(std::str::from_utf8(subslice).expect("Found invalid UTF-8").parse().unwrap());
+                                }
+                                self.start_of_line = true;
+                            } else {
+                                debug!("Got non-NL...");
+                                self.start_of_line = false;
+                            }
+                            self.read_text.push(self.read_buf[0]);
+                        }
+                        Ok(n) => {
+                            warn!("In build loop, received {} bytes", n);
+                            return Err(format!("Build loop stopped reading, received {} bytes", n));
+                        }
+                        Err(e) => {
+                            return Err(format!("Could not read data from keyer: {}", e.to_string()))
+                        }
+                    }
+                }
+            }
+            Err(e) => {
+                return Err(format!("Could not write command to keyer: {}", e.to_string()))
+            }
+        }
     }
 }
 
