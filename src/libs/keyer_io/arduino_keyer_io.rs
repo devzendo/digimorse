aa9e02f0d43135981f775c17b6c2b401cde8d8bc
Matt Gumbley <matt.gumbley@devzendo.org>
2021-03-09 18:36:59 +0000

    Run the keyer decoder in its own thread, in a constant loop, so that keying information can be read (later) and command request/response can be read.

--- src/libs/keyer_io/arduino_keyer_io.rs
+++ src/libs/keyer_io/arduino_keyer_io.rs
@@ -6,6 +6,100 @@ use crate::libs::serial_io::serial_io::SerialIO;
 use crate::libs::util::util::printable;
 use std::thread;
 use std::thread::JoinHandle;
+use std::sync::mpsc::{Sender, Receiver};
+use std::sync::{mpsc, Mutex};
+use std::time::Duration;
+
+pub struct ArduinoKeyer {
+    // Command channel to/from the thread. Sender is guarded by a Mutex to ensure a single command
+    // in flight at a time.
+    command_request_tx: Mutex<Sender<String>>,
+    command_response_rx: Receiver<Result<String, String>>,
+
+    _thread_handle: JoinHandle<()>,
+}
+
+impl ArduinoKeyer {
+    fn new(serial_io: Box<dyn SerialIO>) -> Self {
+        // Channels have two endpoints: the `Sender<T>` and the `Receiver<T>`,
+        // where `T` is the type of the message to be transferred
+        // (type annotation is superfluous)
+        let (command_request_tx, command_request_rx): (Sender<String>, Receiver<String>) = mpsc::channel();
+        let (command_response_tx, command_response_rx): (Sender<Result<String, String>>, Receiver<Result<String, String>>) = mpsc::channel();
+        let mutex_command_request_tx = Mutex::new(command_request_tx);
+
+        let thread_handle = thread::spawn(move || {
+            ArduinoKeyerThread::new(serial_io, command_request_rx, command_response_tx).thread_runner();
+        });
+        Self {
+            command_request_tx: mutex_command_request_tx,
+            command_response_rx,
+            _thread_handle: thread_handle,
+        }
+    }
+
+    fn transact_channels(&self, command: &str) -> Result<String, String> {
+        match self.command_request_tx.lock().unwrap().send(command.to_string()) {
+            Ok(_) => {
+                match self.command_response_rx.recv_timeout(Duration::from_secs(5)) {
+                    Ok(result) => { match result {
+                        Ok(response) => { Ok(response) }
+                        Err(e) => { Err(e) }
+                    } }
+                    Err(timeout) => { Err(format!("Timeout: {}", timeout)) } // not ideal
+                }
+            }
+            Err(send_error) => { Err(format!("SendError: {}", send_error)) }
+        }
+    }
+}
+
+impl Keyer for ArduinoKeyer {
+    fn get_version(&mut self) -> Result<String, String> {
+        let keyer_command = "v\n";
+        self.transact_channels(keyer_command)
+    }
+
+    fn get_speed(&mut self) -> Result<u8, String> {
+        unimplemented!()
+    }
+
+    fn set_speed(&mut self, _wpm: u8) -> Result<(), String> {
+        unimplemented!()
+    }
+
+    fn get_keying_mode(&mut self) -> Result<KeyingMode, String> {
+        unimplemented!()
+    }
+
+    fn set_keying_mode(&mut self, _mode: KeyingMode) -> Result<(), String> {
+        unimplemented!()
+    }
+
+    fn get_keyer_polarity(&mut self) -> Result<KeyerPolarity, String> {
+        unimplemented!()
+    }
+
+    fn set_keyer_polarity(&mut self, _polarity: KeyerPolarity) -> Result<(), String> {
+        unimplemented!()
+    }
+
+    fn get_keyer_output_mode(&mut self) -> Result<KeyerOutputMode, String> {
+        unimplemented!()
+    }
+
+    fn set_keyer_output_mode(&mut self, _mode: KeyerOutputMode) -> Result<(), String> {
+        unimplemented!()
+    }
+
+    fn set_edge_event_listener(&mut self, _pulse_event_listener: &mut dyn KeyingEdgeEventListener) {
+        unimplemented!()
+    }
+
+    fn clear_edge_event_listener(&mut self) {
+        unimplemented!()
+    }
+}
 
 #[derive(Debug)]
 pub enum KeyerState {
@@ -14,63 +108,65 @@ pub enum KeyerState {
     ResponseGotGt, ResponseGotSpc, ResponseFinish
 }
 
-pub struct ArduinoKeyer<'a> {
+struct ArduinoKeyerThread {
     // Low-level serial access
-    serial_io:&'a mut (dyn SerialIO + 'a),
+    serial_io: Box<dyn SerialIO>,
 
-    // Handling I/O from the keyer is done in this thread, which manages the state, the
-    // command response (using read_text buffer and command_response countdown latch), the
-    // notification of keying events (using XXXXX)
-    //thread: Option<JoinHandle<()>>,
+    // Command channels
+    command_request_rx: Receiver<String>,
+    command_response_tx: Sender<Result<String, String>>,
 
     // State machine data
     state: KeyerState,
     read_text: Vec<u8>,
 }
 
-impl<'a> ArduinoKeyer<'a> {
-    fn new(s: &'a mut dyn SerialIO) -> Self {
+impl ArduinoKeyerThread {
+    fn new(serial_io: Box<dyn SerialIO>,
+        command_request_rx: Receiver<String>, command_response_tx: Sender<Result<String, String>>) -> Self {
         Self {
-            serial_io: s,
-            //thread: None, // Use start() to create the thread...
+            serial_io,
+            command_request_rx,
+            command_response_tx,
             state: Initial,
             read_text: vec![],
         }
     }
 
+    // Thread that handles transactions asynchronously...
+    // Requests/Responses cause the transact state machine to trigger, no support yet for
+    // Notifications.
     fn thread_runner(&mut self) -> () {
         debug!("Keyer I/O thread started");
+        // TODO until poisoned?
+        loop {
+            // Any incoming commands?
+            match self.command_request_rx.try_recv() {
+                Ok(command) => {
+                    let response_result = self.transact_serial(command.as_str());
+                    match self.command_response_tx.send(response_result) {
+                        Ok(_) => {}
+                        Err(_) => {}
+                    }
+                }
+                Err(_) => {
+                    // could timeout, or be disconnected?
+                    // ignore for now...
+                }
+            }
+            // Any keyer data?
 
-        debug!("Keyer I/O thread stopped");
+        }
+        // TODO when we swallow poison, exit here.
+        //debug!("Keyer I/O thread stopped");
     }
 
-    fn start(&mut self) {
-        debug!("Starting I/O thread...");
-        // Thread that handles transactions asynchronously...
-        // Requests/Responses cause the transact state machine to trigger, no support yet for
-        // Notifications.
-/*        self.thread = Some(thread::Builder::new()
-            .name("Keyer I/O thread".into())
-            .spawn(|| {
-                let handle = thread::current();
-                self.thread_runner();
-            })
-            .unwrap());
-
- */
-        debug!("... started I/O thread");
-    }
-
-    fn transact(&mut self, command_to_keyer: &str) -> Result<String, String> {
-        // command_mutex.acquire();
-        // command_response.allow(1);
+    fn transact_serial(&mut self, command_to_keyer: &str) -> Result<String, String> {
+        debug!("Transact command [{}]", command_to_keyer);
         let written_bytes = self.serial_io.write(command_to_keyer.as_bytes());
         match written_bytes {
             Ok(n) => {
                 debug!("Written {} bytes to keyer", n);
-                // command_response.await();
-                // response = get_response(); // from read_text
-                // command_mutex.release();
                 self.set_state(Initial);
                 let mut read_buf: [u8; 1] = [0];
 
@@ -102,6 +198,7 @@ impl<'a> ArduinoKeyer<'a> {
                             match next {
                                 // A return of some type is needed
                                 Some(result) => {
+                                    debug!("Transact returning [{:?}]", result);
                                     return result;
                                 }
                                 // State may have changed, stay in here, read more...
@@ -188,63 +285,14 @@ impl<'a> ArduinoKeyer<'a> {
             }
             b'\n' => {
                 self.set_state(Initial);
-                let mut subslice = &self.read_text[0..self.read_text.len()];
+                let subslice = &self.read_text[0..self.read_text.len()];
                 Some(Ok(String::from_utf8(Vec::from(subslice)).expect("Found invalid UTF-8")))
-                // command_response.count_down();
             }
             _ => {
                 warn!("Unexpected response data {}", printable(ch));
                 Some(Err(format!("Unexpected response data {}", printable(ch))))
             }
         }
-    }
-}
-
-impl<'a> Keyer for ArduinoKeyer<'a> {
-    fn get_version(&mut self) -> Result<String, String> {
-        let keyer_will_send = "v\n";
-
-        self.transact(keyer_will_send)
-    }
-
-    fn get_speed(&mut self) -> Result<u8, String> {
-        unimplemented!()
-    }
-
-    fn set_speed(&mut self, _wpm: u8) -> Result<(), String> {
-        unimplemented!()
-    }
-
-    fn get_keying_mode(&mut self) -> Result<KeyingMode, String> {
-        unimplemented!()
-    }
-
-    fn set_keying_mode(&mut self, _mode: KeyingMode) -> Result<(), String> {
-        unimplemented!()
-    }
-
-    fn get_keyer_polarity(&mut self) -> Result<KeyerPolarity, String> {
-        unimplemented!()
-    }
-
-    fn set_keyer_polarity(&mut self, _polarity: KeyerPolarity) -> Result<(), String> {
-        unimplemented!()
-    }
-
-    fn get_keyer_output_mode(&mut self) -> Result<KeyerOutputMode, String> {
-        unimplemented!()
-    }
-
-    fn set_keyer_output_mode(&mut self, _mode: KeyerOutputMode) -> Result<(), String> {
-        unimplemented!()
-    }
-
-    fn set_edge_event_listener(&mut self, _pulse_event_listener: &mut dyn KeyingEdgeEventListener) {
-        unimplemented!()
-    }
-
-    fn clear_edge_event_listener(&mut self) {
-        unimplemented!()
     }
 }
 
--- src/libs/keyer_io/arduino_keyer_io_spec.rs
+++ src/libs/keyer_io/arduino_keyer_io_spec.rs
@@ -5,21 +5,19 @@ use crate::libs::serial_io::serial_io::SerialIO;
 use crate::libs::util::util::*;
 use std::io;
 use std::io::{Error, ErrorKind};
+use std::sync::mpsc::{Sender};
 
 struct FakeSerialIO {
     playback_chars: Vec<u8>,
     playback_index: usize,
-    record_chars: Vec<u8>
+
+    // Returns whatever has been sent by higher levels (the keyer's command sending routine).
+    recording_tx: Sender<u8>
 }
 
 impl FakeSerialIO {
-    fn new(playback: String) -> Self {
-        Self { playback_chars: playback.into_bytes(), playback_index: 0, record_chars: vec![] }
-    }
-
-    // Returns whatever has been sent by higher levels (the keyer's command sending routine).
-    fn recording(&self) -> String {
-        return String::from_utf8(Vec::from(self.record_chars.as_slice())).expect("Found invalid UTF-8")
+    fn new(playback: String, recording_tx: Sender<u8>) -> Self {
+        Self { playback_chars: playback.into_bytes(), playback_index: 0, recording_tx }
     }
 }
 
@@ -41,7 +39,10 @@ impl SerialIO for FakeSerialIO {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
         for n in 0..buf.len() {
             debug!("transmitted {}", printable(buf[n]));
-            self.record_chars.push(buf[n]);
+            match self.recording_tx.send(buf[n]) {
+                Ok(_) => {}
+                Err(_) => {}
+            }
         }
         return Ok(buf.len())
     }
@@ -53,11 +54,11 @@ impl SerialIO for FakeSerialIO {
 
 #[cfg(test)]
 mod arduino_keyer_io_spec {
-    //use hamcrest2::prelude::*;
     use crate::libs::keyer_io::arduino_keyer_io::arduino_keyer_io_spec::FakeSerialIO;
     use crate::libs::keyer_io::arduino_keyer_io::ArduinoKeyer;
     use crate::libs::keyer_io::keyer_io::Keyer;
-    use crate::libs::serial_io::serial_io::SerialIO;
+    use std::sync::mpsc::{Sender, Receiver};
+    use std::sync::mpsc;
 
     #[ctor::ctor]
     fn before_each() {
@@ -74,16 +75,24 @@ mod arduino_keyer_io_spec {
         let keyer_will_send = "v\n";
         let keyer_will_receive = "> v1.0.0\n\n";
 
-        let mut serial_io = FakeSerialIO::new(keyer_will_receive.to_string());
-        let mut keyer = ArduinoKeyer::new(& mut serial_io as & mut dyn SerialIO);
+        let (recording_tx, recording_rx): (Sender<u8>, Receiver<u8>) = mpsc::channel();
+
+        let serial_io = FakeSerialIO::new(keyer_will_receive.to_string(), recording_tx);
+        let mut keyer = ArduinoKeyer::new(Box::new(serial_io));
         match keyer.get_version() {
             Ok(v) => {
+                // Keyer replied with....
                 assert_eq!(v, "v1.0.0");
             }
             Err(e) => {
                 panic!("Did not get version: {}", e);
             }
         }
-        assert_eq!(serial_io.recording(), keyer_will_send.to_string());
+
+        // Keyer was sent...
+        let iter = recording_rx.try_iter();
+        let recording: Vec<u8> = iter.collect();
+        let recording_string = String::from_utf8(recording).expect("Found invalid UTF-8");
+        assert_eq!(recording_string, keyer_will_send.to_string());
     }
 }
