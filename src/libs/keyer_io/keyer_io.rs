6260555bfff461fa067307b681192cf64b5ef16a
Matt Gumbley <matt.gumbley@devzendo.org>
2021-05-22 22:04:16 +0100

    Fixed Start, End, Keying event processing. Remove unused 'edge' output mode.

--- src/libs/keyer_io/keyer_io.rs
+++ src/libs/keyer_io/keyer_io.rs
@@ -1,6 +1,6 @@
-use std::fmt::{Display, Formatter};
+use std::fmt::{Display, Formatter, Debug};
 use std::fmt;
-use crate::libs::keyer_io::keyer_io::KeyingEvent::{Edge, Timed};
+use crate::libs::keyer_io::keyer_io::KeyingEvent::{Timed, Start, End};
 
 pub enum KeyingMode {
     Straight, Paddle, // add Iambic A, Iambic B etc. later
@@ -10,54 +10,66 @@ pub enum KeyerPolarity {
     Normal, Reverse
 }
 
-pub enum KeyerOutputMode {
-    Edge, Timing
-}
-
 // Speed in WPM
 pub type KeyerSpeed = u8;
 
-// A keying edge (key down, key up) has just been detected. It's up to the receiver of this event
-// to work out the duration of the up/down press/release - since the receipt of the last one.
-#[derive(Clone)]
-pub struct KeyingEdgeEvent {
-    pub up: bool,
-}
-impl Display for KeyingEdgeEvent {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let c = if self.up { '+' } else { '-' };
-        write!(f, "EDGE {}", c)
-    }
-}
-
 // A keying edge with duration.
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub struct KeyingTimedEvent {
     pub up: bool, // The edge that has transitioned, key up or key down
     pub duration: KeyerEdgeDurationMs, // How long this edge lasted for
 }
+
 pub type KeyerEdgeDurationMs = u16; // 20ms is 60WPM dit, 720ms is 5WPM dah
+
 impl Display for KeyingTimedEvent {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        let c = if self.up { '+' } else { '-' };
+        let c = if self.up { '-' } else { '+' };
         write!(f, "TIMED {} {}", c, self.duration)
     }
 }
 
-#[derive(Clone)]
+impl Debug for KeyingTimedEvent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        let c = if self.up { '-' } else { '+' };
+        write!(f, "TIMED {} {}", c, self.duration)
+    }
+}
+
+#[derive(Clone, PartialEq)]
 pub enum KeyingEvent {
-    Edge(KeyingEdgeEvent),
     Timed(KeyingTimedEvent),
+    Start(),
+    End(),
 }
 
 impl Display for KeyingEvent {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self {
-            Edge(edge) => {
-                write!(f, "{}", edge)
-            }
             Timed(timed) => {
                 write!(f, "{}", timed)
+            }
+            Start() => {
+                write!(f, "START")
+            }
+            End() => {
+                write!(f, "END")
+            }
+        }
+    }
+}
+
+impl Debug for KeyingEvent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self {
+            Timed(timed) => {
+                write!(f, "{}", timed)
+            }
+            Start() => {
+                write!(f, "START")
+            }
+            End() => {
+                write!(f, "END")
             }
         }
     }
@@ -74,8 +86,5 @@ pub trait Keyer {
 
     fn get_keyer_polarity(&mut self) -> Result<KeyerPolarity, String>;
     fn set_keyer_polarity(&mut self, polarity: KeyerPolarity)-> Result<(), String>;
-
-    fn get_keyer_output_mode(&mut self) -> Result<KeyerOutputMode, String>;
-    fn set_keyer_output_mode(&mut self, mode: KeyerOutputMode)-> Result<(), String>;
 }
 
