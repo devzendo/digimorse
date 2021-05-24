80950f023c82ace23e79498a2d4f93a3af0dd2be
Matt Gumbley <matt.gumbley@devzendo.org>
2021-05-24 20:49:58 +0100

    Start parsing the command line.

--- src/digimorse/bin/main.rs
+++ src/digimorse/bin/main.rs
@@ -1,17 +1,54 @@
+#[macro_use]
 extern crate clap;
 
-use clap::{App, /*Arg,*/ ArgMatches};
+use clap::{App, Arg, ArgMatches};
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
-fn parse_command_line<'a>() -> ArgMatches<'a> {
-    return App::new("digimorse")
+#[cfg(windows)]
+const KEYER_HELP: &str = "Sets the port that the Digimorse Arduino Keyer is connected to, e.g. COM4:";
+#[cfg(windows)]
+const KEYER_VALUE_NAME: &str = "COM port";
+
+#[cfg(not(windows))]
+const KEYER_HELP: &str = "Sets the port that the Digimorse Arduino Keyer is connected to, e.g. /dev/cu-usbserial-1410";
+#[cfg(not(windows))]
+const KEYER_VALUE_NAME: &str = "Serial character device";
+
+// TODO Not sure what a suitable port name for Linux would be
+
+arg_enum! {
+    #[derive(Debug)]
+    enum Mode {
+        GUI,
+        KeyerDiag,
+        SourceEncoderDiag
+    }
+}
+
+fn parse_command_line<'a>() -> (ArgMatches<'a>, Mode) {
+    let result = App::new("digimorse")
         .version(VERSION)
         .author("Matt Gumbley <matt.gumbley@gmail.com>")
         .about("Digitally Encoded Morse Transceiver")
+
+        .arg(Arg::from_usage("<mode> 'The mode to use, usually GUI.'").possible_values(&Mode::variants()).default_value("GUI"))
+
+        .arg(Arg::with_name("keyer port")
+            .short("k")
+            .long("keyer")
+            .value_name(KEYER_VALUE_NAME)
+            .help(KEYER_HELP)
+            .takes_value(true))
+
         .get_matches();
+
+    let mode = value_t!(result.value_of("mode"), Mode).unwrap_or(Mode::GUI);
+
+    return (result, mode);
 }
 
 fn main() {
-    let _arguments = parse_command_line();
+    let (arguments, mode) = parse_command_line();
+    println!("Command line parsed");
 }
