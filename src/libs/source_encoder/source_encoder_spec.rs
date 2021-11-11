9f67355d86fad6e28ba0b95961345827fbf49963
Matt Gumbley <matt.gumbley@devzendo.org>
2021-11-11 09:00:34 +0000

    Add real keying events, WIP

--- src/libs/source_encoder/source_encoder_spec.rs
+++ src/libs/source_encoder/source_encoder_spec.rs
@@ -11,6 +11,8 @@ mod source_encoder_spec {
     use std::env;
     use std::sync::Arc;
     use std::sync::atomic::AtomicBool;
+    use std::time::Duration;
+    use crate::libs::util::test_util;
 
     #[ctor::ctor]
     fn before_each() {
@@ -72,45 +74,54 @@ mod source_encoder_spec {
 
     #[rstest]
     fn encode_keying(mut fixture: SourceEncoderFixture) {
-        // define new encoding event, a type alias of vec u8?
-        // create a encoding_tx, encoding_rx mpsc::channel and pass the encoding_tx to the encoder.
-        // the loop below reads encodings and puts them in a vec for testing.
-        // then inject some keyings
-        let keyer_speed: KeyerSpeed = 20;
-        fixture.source_encoder.set_keyer_speed(keyer_speed);
+        test_util::panic_after(Duration::from_secs(2), move|| {
 
-        // inject these keyings...
-        let keyings = vec![
-            KeyingEvent::Start(),
+            // define new encoding event, a type alias of vec u8?
+            // the loop below reads encodings and puts them in a vec for testing.
+            // then inject some keyings
+            let keyer_speed: KeyerSpeed = 20;
+            fixture.source_encoder.set_keyer_speed(keyer_speed);
 
-            KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 10 }),
-            KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 10 }),
-            KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 30 }),
-            KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 10 }),
-            KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 30 }),
-            KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 10 }),
-            KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 10 }),
-
-            KeyingEvent::End()
-        ];
-        // for k in keyings {
-        //     fixture.keying_event_tx.broadcast(k);
-        // }
-        // // Force the encoder to emit a frame
-        // fixture.source_encoder.emit();
-        // let result = fixture.source_encoder_rx.recv();
-        // match result {
-        //     Ok(source_encoding) => {
-        //         info!("encode_keying: isEnd {}", source_encoding.isEnd);
-        //         let hexdump = pretty_hex(&source_encoding.frame);
-        //         let hexdump_lines = hexdump.split("\n");
-        //         for line in hexdump_lines {
-        //             info!("encode_keying: Encoding {}", line);
-        //         }
-        //     }
-        //     Err(err) => {
-        //         panic!("encode_keying: error reading encoder bus {}", err);
-        //     }
-        // }
+            // inject these keyings...
+            let keyings = vec![
+                KeyingEvent::Start(),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 210 }), // C
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 72 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 73 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 64 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 250 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 65 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 61 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 298 }), // inter-letter gap
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 234 }),  // Q
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 45 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 208 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 77 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 78 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 56 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: true, duration: 323 }),
+                KeyingEvent::Timed(KeyingTimedEvent { up: false, duration: 596 }), // inter-word gap
+                KeyingEvent::End()
+            ];
+            for k in keyings {
+                fixture.keying_event_tx.broadcast(k);
+            }
+            // Force the encoder to emit a frame
+            fixture.source_encoder.emit();
+            let result = fixture.source_encoder_rx.recv();
+            match result {
+                Ok(source_encoding) => {
+                    info!("encode_keying: isEnd {}", source_encoding.isEnd);
+                    let hexdump = pretty_hex(&source_encoding.frame);
+                    let hexdump_lines = hexdump.split("\n");
+                    for line in hexdump_lines {
+                        info!("encode_keying: Encoding {}", line);
+                    }
+                }
+                Err(err) => {
+                    panic!("encode_keying: error reading encoder bus {}", err);
+                }
+            }
+        })
     }
 }
