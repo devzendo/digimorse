1abf307c2c2563d6482dd19dcadaf73ce7f32baa
Matt Gumbley <matt.gumbley@devzendo.org>
2021-11-20 22:39:46 +0000

    Extract function.

--- src/libs/source_encoder/bitvec_source_encoding_builder.rs
+++ src/libs/source_encoder/bitvec_source_encoding_builder.rs
@@ -22,6 +22,20 @@ impl BitvecSourceEncodingBuilder {
             panic!("Adding {} bit(s) would exhaust storage", num_bits_being_added);
         }
     }
+
+    fn pack_data_bits<D: BitStore>(&mut self, data: &mut D, num_bits: usize, max_bits: usize) {
+        // The code works without this optimisation.
+        if num_bits == 0 {
+            return;
+        }
+        self.panic_if_full(num_bits);
+        let data_slice = BitSlice::<Msb0, _>::from_element_mut(data);
+        unsafe {
+            let data_sub_slice = data_slice.get_unchecked_mut((max_bits - num_bits)..max_bits);
+            let mut data_sub_bit_vec = data_sub_slice.to_bitvec();
+            self.bits.append(&mut data_sub_bit_vec);
+        }
+    }
 }
 
 impl SourceEncodingBuilder for BitvecSourceEncodingBuilder {
@@ -34,54 +48,24 @@ impl SourceEncodingBuilder for BitvecSourceEncodingBuilder {
     }
 
     fn add_8_bits(&mut self, mut data: u8, num_bits: usize) {
-        // The code works without this optimisation.
-        if num_bits == 0 {
-            return;
-        }
         if num_bits > 8 {
             panic!("Cannot add more than 8 bits with add_8_bits, was trying to add {}", num_bits);
         }
-        self.panic_if_full(num_bits);
-        let data_slice = BitSlice::<Msb0, _>::from_element_mut(&mut data);
-        unsafe {
-            let data_sub_slice = data_slice.get_unchecked_mut((8 - num_bits)..8);
-            let mut data_sub_bit_vec = data_sub_slice.to_bitvec();
-            self.bits.append(&mut data_sub_bit_vec);
-        }
+        self.pack_data_bits::<u8>(&mut data, num_bits, 8);
     }
 
     fn add_16_bits(&mut self, mut data: u16, num_bits: usize) {
-        // The code works without this optimisation.
-        if num_bits == 0 {
-            return;
-        }
         if num_bits > 16 {
             panic!("Cannot add more than 16 bits with add_16_bits, was trying to add {}", num_bits);
         }
-        self.panic_if_full(num_bits);
-        let data_slice = BitSlice::<Msb0, _>::from_element_mut(&mut data);
-        unsafe {
-            let data_sub_slice = data_slice.get_unchecked_mut((16 - num_bits)..16);
-            let mut data_sub_bit_vec = data_sub_slice.to_bitvec();
-            self.bits.append(&mut data_sub_bit_vec);
-        }
+        self.pack_data_bits::<u16>(&mut data, num_bits, 16);
     }
 
     fn add_32_bits(&mut self, mut data: u32, num_bits: usize) {
-        // The code works without this optimisation.
-        if num_bits == 0 {
-            return;
-        }
         if num_bits > 32 {
             panic!("Cannot add more than 32 bits with add_32_bits, was trying to add {}", num_bits);
         }
-        self.panic_if_full(num_bits);
-        let data_slice = BitSlice::<Msb0, _>::from_element_mut(&mut data);
-        unsafe {
-            let data_sub_slice = data_slice.get_unchecked_mut((32 - num_bits)..32);
-            let mut data_sub_bit_vec = data_sub_slice.to_bitvec();
-            self.bits.append(&mut data_sub_bit_vec);
-        }
+        self.pack_data_bits::<u32>(&mut data, num_bits, 32);
     }
 
     fn add_bool(&mut self, data: bool) {
