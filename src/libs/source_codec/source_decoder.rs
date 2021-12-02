b021cb8ed8afd47451d400662a59961b55cbabf0
Matt Gumbley <matt.gumbley@devzendo.org>
2021-12-02 21:25:02 +0000

    Start the source decoder.

--- src/libs/source_codec/source_decoder.rs
+++ src/libs/source_codec/source_decoder.rs
@@ -1,11 +1,17 @@
+use bitvec::prelude::*;
 use std::error::Error;
-use crate::libs::source_codec::source_encoding::Frame;
+use crate::libs::source_codec::source_encoding::{Frame, SOURCE_ENCODER_BLOCK_SIZE_IN_BITS};
 
 /// Decode an encoded block. This is either error-free or has been extracted from an error-corrected
 /// block; i.e. no decode errors should occur. Just in case there's a decode error, this returns a
 /// Result.
 pub fn source_decode(encoded_block: Vec<u8>) -> Result<Vec<Frame>, Box<dyn Error>> {
+    if encoded_block.len() != (SOURCE_ENCODER_BLOCK_SIZE_IN_BITS >> 3) {
+        return Err(Box::<dyn Error + Send + Sync>::from(format!("Cannot decode a block of the wrong size")));
+    }
     let frames: Vec<Frame> = vec![];
+    let mut bit_vec = BitVec::<Msb0, u8>::with_capacity(SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
+
     Ok(frames)
 }
 
--- src/libs/source_codec/source_decoder_spec.rs
+++ src/libs/source_codec/source_decoder_spec.rs
@@ -2,6 +2,7 @@ extern crate hamcrest2;
 
 #[cfg(test)]
 mod source_decoder_spec {
+    use log::info;
     use std::env;
     use crate::libs::source_codec::source_decoder::source_decode;
     use crate::libs::source_codec::source_encoding::Frame;
@@ -21,6 +22,27 @@ mod source_decoder_spec {
         let block = encoded(20, &[]);
         let expected_frames = vec![];
         assert_decoded_eq(block, expected_frames)
+    }
+
+    #[test]
+    pub fn decode_complete_emptiness() {
+        should_decode_with_error(vec![])
+    }
+
+    #[test]
+    pub fn decode_wrong_size() {
+        should_decode_with_error(vec![0, 0]);
+    }
+
+    fn should_decode_with_error(block: Vec<u8>) {
+        match source_decode(block) {
+            Ok(frames) => {
+                panic!("Should not have successfully decoded")
+            }
+            Err(e) => {
+                info!("Expected error: {}", e);
+            }
+        }
     }
 
     fn assert_decoded_eq(block: Vec<u8>, expected_frames: Vec<Frame>) {
