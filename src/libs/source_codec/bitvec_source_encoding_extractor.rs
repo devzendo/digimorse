bc5c9aeb243cadf32b996e99a60aad3eca31bdcf
Matt Gumbley <matt.gumbley@devzendo.org>
2021-12-08 10:27:43 +0000

    SourceEncodingExtractor extract_16_bits and extract_32_bits.

--- src/libs/source_codec/bitvec_source_encoding_extractor.rs
+++ src/libs/source_codec/bitvec_source_encoding_extractor.rs
@@ -57,11 +57,55 @@ impl SourceEncodingExtractor for BitvecSourceEncodingExtractor {
     }
 
     fn extract_16_bits(&mut self, num_bits: usize) -> u16 {
-        todo!()
+        // Code works without this optimisation
+        if num_bits == 0 {
+            return 0;
+        }
+        if num_bits > 16 {
+            panic!("Cannot extract more than 16 bits with extract_16_bits");
+        }
+        let remaining = self.bits.len();
+        if remaining < num_bits {
+            panic!("Cannot extract {} bits; {} bits remain", num_bits, remaining);
+        }
+        let mut extracted = self.drain(num_bits);
+        // debug!("extracted {:#018b}", extracted);
+
+        // Perhaps there's a quicker way to fill the vector with 16-num_bits 0's?
+        let mut out_bitvec = BitVec::<Msb0, u16>::new();
+        for _ in 0 .. (16 - num_bits) {
+            out_bitvec.push(false);
+        }
+        out_bitvec.append(&mut extracted);
+
+        // debug!("out_bitvec len {} {:#016b}", out_bitvec.len(), out_bitvec);
+        out_bitvec.as_raw_slice()[0]
     }
 
     fn extract_32_bits(&mut self, num_bits: usize) -> u32 {
-        todo!()
+        // Code works without this optimisation
+        if num_bits == 0 {
+            return 0;
+        }
+        if num_bits > 32 {
+            panic!("Cannot extract more than 32 bits with extract_32_bits");
+        }
+        let remaining = self.bits.len();
+        if remaining < num_bits {
+            panic!("Cannot extract {} bits; {} bits remain", num_bits, remaining);
+        }
+        let mut extracted = self.drain(num_bits);
+        // debug!("extracted {:#034b}", extracted);
+
+        // Perhaps there's a quicker way to fill the vector with 32-num_bits 0's?
+        let mut out_bitvec = BitVec::<Msb0, u32>::new();
+        for _ in 0 .. (32 - num_bits) {
+            out_bitvec.push(false);
+        }
+        out_bitvec.append(&mut extracted);
+
+        // debug!("out_bitvec len {} {:#034b}", out_bitvec.len(), out_bitvec);
+        out_bitvec.as_raw_slice()[0]
     }
 }
 /*
--- src/libs/source_codec/bitvec_source_encoding_extractor_spec.rs
+++ src/libs/source_codec/bitvec_source_encoding_extractor_spec.rs
@@ -47,6 +47,8 @@ mod bitvec_source_encoding_extractor_spec {
         assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
     }
 
+    // extract_8_bits ------------------------------------------------------------------------------
+
     #[test]
     pub fn extract_8_by_zero_keeps_remaining() {
         let mut extractor = extractor(vec![0b10000000, 0, 0, 0, 0, 0, 0, 0]);
@@ -109,5 +111,147 @@ mod bitvec_source_encoding_extractor_spec {
         extractor.extract_8_bits(8);
         assert_eq!(extractor.remaining(), 0);
         extractor.extract_8_bits(1); // boom
+    }
+
+    // extract_16_bits -----------------------------------------------------------------------------
+
+    #[test]
+    pub fn extract_16_by_zero_keeps_remaining() {
+        let mut extractor = extractor(vec![0b10000000, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(extractor.extract_16_bits(0), 0b0000000000000000);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
+    }
+
+    #[test]
+    pub fn extract_16_by_a_bit_reduces_remaining() {
+        let mut extractor = extractor(vec![0b10000000, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(extractor.extract_16_bits(1), 0b0000000000000001);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 1);
+    }
+
+    #[test]
+    pub fn extract_16_by_4_bits_reduces_remaining() {
+        let mut extractor = extractor(vec![0b10110000, 0, 0, 0, 0, 0, 0, 0]);
+        let extracted = extractor.extract_16_bits(4);
+        debug!("test extracted {:#018b}", extracted);
+        assert_eq!(extracted, 0b0000000000001011);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 4);
+    }
+
+    #[test]
+    pub fn extract_16_by_8_bits_retains_bit_ordering() {
+        let mut extractor = extractor(vec![0b10110001, 0, 0, 0, 0, 0, 0, 0]);
+        let extracted = extractor.extract_16_bits(8);
+        debug!("test extracted {:#018b}", extracted);
+
+        assert_eq!(extracted, 0b0000000010110001);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 8);
+    }
+
+    #[test]
+    pub fn extract_16_by_16_bits_retains_bit_ordering() {
+        let mut extractor = extractor(vec![0b10110001, 0b00110011, 0, 0, 0, 0, 0, 0]);
+        let extracted = extractor.extract_16_bits(16);
+        debug!("test extracted {:#018b}", extracted);
+
+        assert_eq!(extracted, 0b1011000100110011);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 16);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn extract_16_too_much_panics() {
+        let mut extractor = extractor(vec![0b10110001, 0, 0, 0, 0, 0, 0, 0]);
+        extractor.extract_16_bits(17);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn extract_16_extract_too_much() {
+        let mut extractor = extractor(vec![0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
+        extractor.extract_16_bits(16);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 16);
+        extractor.extract_16_bits(16);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 32);
+        extractor.extract_16_bits(16);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 48);
+        extractor.extract_16_bits(16);
+        assert_eq!(extractor.remaining(), 0);
+        extractor.extract_16_bits(1); // boom
+    }
+
+    // extract_32_bits -----------------------------------------------------------------------------
+
+    #[test]
+    pub fn extract_32_by_zero_keeps_remaining() {
+        let mut extractor = extractor(vec![0b10000000, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(extractor.extract_32_bits(0), 0b0000000000000000);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
+    }
+
+    #[test]
+    pub fn extract_32_by_a_bit_reduces_remaining() {
+        let mut extractor = extractor(vec![0b10000000, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(extractor.extract_32_bits(1), 0b00000000000000000000000000000001);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 1);
+    }
+
+    #[test]
+    pub fn extract_32_by_4_bits_reduces_remaining() {
+        let mut extractor = extractor(vec![0b10110000, 0, 0, 0, 0, 0, 0, 0]);
+        let extracted = extractor.extract_32_bits(4);
+        debug!("test extracted {:#034b}", extracted);
+        assert_eq!(extracted, 0b00000000000000000000000000001011);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 4);
+    }
+
+    #[test]
+    pub fn extract_32_by_8_bits_retains_bit_ordering() {
+        let mut extractor = extractor(vec![0b10110001, 0, 0, 0, 0, 0, 0, 0]);
+        let extracted = extractor.extract_32_bits(8);
+        debug!("test extracted {:#034b}", extracted);
+
+        assert_eq!(extracted, 0b00000000000000000000000010110001);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 8);
+    }
+
+    #[test]
+    pub fn extract_32_by_16_bits_retains_bit_ordering() {
+        let mut extractor = extractor(vec![0b10110001, 0b00110011, 0, 0, 0, 0, 0, 0]);
+        let extracted = extractor.extract_32_bits(16);
+        debug!("test extracted {:#034b}", extracted);
+
+        assert_eq!(extracted, 0b00000000000000001011000100110011);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 16);
+    }
+
+    #[test]
+    pub fn extract_32_by_32_bits_retains_bit_ordering() {
+        let mut extractor = extractor(vec![0b10110001, 0b00110011, 0b01010101, 0b11110000, 0, 0, 0, 0]);
+        let extracted = extractor.extract_32_bits(32);
+        debug!("test extracted {:#034b}", extracted);
+
+        assert_eq!(extracted, 0b10110001001100110101010111110000);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 32);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn extract_32_too_much_panics() {
+        let mut extractor = extractor(vec![0b10110001, 0, 0, 0, 0, 0, 0, 0]);
+        extractor.extract_32_bits(33);
+    }
+
+    #[test]
+    #[should_panic]
+    pub fn extract_32_extract_too_much() {
+        let mut extractor = extractor(vec![0, 0, 0, 0, 0, 0, 0, 0]);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
+        extractor.extract_32_bits(32);
+        assert_eq!(extractor.remaining(), SOURCE_ENCODER_BLOCK_SIZE_IN_BITS - 32);
+        extractor.extract_32_bits(32);
+        assert_eq!(extractor.remaining(), 0);
+        extractor.extract_16_bits(1); // boom
     }
 }
