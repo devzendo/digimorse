1860931f7af0e7fbb0d49cd9ad35ae904658853c
Matt Gumbley <matt.gumbley@devzendo.org>
2021-12-09 12:07:36 +0000

    Implement decoding for all keying-related frames. Metadata will be in a later phase.

--- src/libs/source_codec/source_decoder.rs
+++ src/libs/source_codec/source_decoder.rs
@@ -1,6 +1,11 @@
-use bitvec::prelude::*;
+use log::debug;
 use std::error::Error;
-use crate::libs::source_codec::source_encoding::{Frame, SOURCE_ENCODER_BLOCK_SIZE_IN_BITS};
+use crate::enum_primitive::FromPrimitive;
+use crate::libs::source_codec::bitvec_source_encoding_extractor::BitvecSourceEncodingExtractor;
+use crate::libs::source_codec::keying_encoder::decode_from_binary_with_known_sign;
+use crate::libs::source_codec::keying_timing::{DefaultKeyingTiming, KeyingTiming};
+use crate::libs::source_codec::source_encoding::{EncoderFrameType, Frame, SOURCE_ENCODER_BLOCK_SIZE_IN_BITS, SourceEncodingExtractor};
+use crate::libs::util::util::dump_byte_vec;
 
 /// Decode an encoded block. This is either error-free or has been extracted from an error-corrected
 /// block; i.e. no decode errors should occur. Just in case there's a decode error, this returns a
@@ -9,12 +14,125 @@ pub fn source_decode(encoded_block: Vec<u8>) -> Result<Vec<Frame>, Box<dyn Error
     if encoded_block.len() != (SOURCE_ENCODER_BLOCK_SIZE_IN_BITS >> 3) {
         return Err(Box::<dyn Error + Send + Sync>::from(format!("Cannot decode a block of the wrong size")));
     }
-    let frames: Vec<Frame> = vec![];
-    let mut bit_vec = BitVec::<Msb0, u8>::with_capacity(SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
-
+    debug!("Decoding {}", dump_byte_vec(&encoded_block));
+    let no_wpm_polarity_err = Err(Box::<dyn Error>::from("Cannot decode keying without prior WPM|Polarity"));
+    let mut timing = DefaultKeyingTiming::new();
+    let mut seen_wpm_polarity = false;
+    let mut frames: Vec<Frame> = vec![];
+    let mut extractor = BitvecSourceEncodingExtractor::new(encoded_block);
+    loop {
+        let remaining = extractor.remaining();
+        if remaining < 4 {
+            debug!("End of decode: Insufficient data ({} bits) to read field type", remaining);
+            break;
+        }
+        let field_type_nibble= extractor.extract_8_bits(4);
+        let maybe_field_type = EncoderFrameType::from_u8(field_type_nibble);
+        match maybe_field_type {
+            None => {
+                // Cannot happen, since we've extracted 4 bits and they're all valid EncoderFrameTypes.
+                panic!("Cannot interpret {:#010b} as an EncoderFrameType", field_type_nibble);
+            }
+            // Note: The source encoder ensures that frames are atomic, that is, you won't find a
+            // frame header without its data. If the frame would not fit at the end of a block, it
+            // will be placed in the next block.
+            Some(field_type) => {
+                debug!("Decoding field {:?}", field_type);
+                match field_type {
+                    EncoderFrameType::Padding => {
+                        frames.push(Frame::Padding);
+                        // No need to decode further, padding indicates end of block.
+                        debug!("End of decode: Padding");
+                        break;
+                    }
+                    EncoderFrameType::WPMPolarity => {
+                        seen_wpm_polarity = true;
+                        let keying_speed = extractor.extract_8_bits(6);
+                        let mark = extractor.extract_bool();
+                        timing.set_keyer_speed(keying_speed);
+                        frames.push(Frame::WPMPolarity { wpm: keying_speed, polarity: mark });
+                    }
+                    EncoderFrameType::CallsignMetadata => {}
+                    EncoderFrameType::CallsignHashMetadata => {}
+                    EncoderFrameType::LocatorMetadata => {}
+                    EncoderFrameType::PowerMetadata => {}
+                    EncoderFrameType::KeyingPerfectDit => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        frames.push(Frame::KeyingPerfectDit);
+                    }
+                    EncoderFrameType::KeyingPerfectDah => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        frames.push(Frame::KeyingPerfectDah);
+                    }
+                    EncoderFrameType::KeyingPerfectWordgap => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        frames.push(Frame::KeyingPerfectWordgap);
+                    }
+                    EncoderFrameType::KeyingEnd => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        frames.push(Frame::KeyingEnd);
+                    }
+                    EncoderFrameType::KeyingDeltaDit => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        let delta = extract_sized_delta(&mut extractor, timing.dit_encoding_range());
+                        frames.push(Frame::KeyingDeltaDit { delta });
+                    }
+                    EncoderFrameType::KeyingDeltaDah => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        let delta = extract_sized_delta(&mut extractor, timing.dah_encoding_range());
+                        frames.push(Frame::KeyingDeltaDah { delta });
+                    }
+                    EncoderFrameType::KeyingDeltaWordgap => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        let delta = extract_sized_delta(&mut extractor, timing.wordgap_encoding_range());
+                        frames.push(Frame::KeyingDeltaWordgap { delta });
+                    }
+                    EncoderFrameType::KeyingNaive => {
+                        if !seen_wpm_polarity {
+                            return no_wpm_polarity_err;
+                        }
+                        let duration = extractor.extract_16_bits(11);
+                        frames.push(Frame::KeyingNaive { duration });
+                    }
+                    EncoderFrameType::Unused => {
+                        frames.push(Frame::Unused);
+                    }
+                    EncoderFrameType::Extension => {
+                        frames.push(Frame::Extension);
+                    }
+                }
+            }
+        }
+    }
+    debug!("Decoded {:?}", frames);
     Ok(frames)
 }
 
+fn extract_sized_delta(extractor: &mut BitvecSourceEncodingExtractor, encoding_range: (usize, usize)) -> i16 {
+    let negative = extractor.extract_bool();
+    let bits = if negative {
+        encoding_range.0
+    } else {
+        encoding_range.1
+    };
+    let encoded = extractor.extract_16_bits(bits);
+    let delta = decode_from_binary_with_known_sign(encoded, bits, !negative);
+    delta
+}
 
 
 #[cfg(test)]
--- src/libs/source_codec/source_decoder_spec.rs
+++ src/libs/source_codec/source_decoder_spec.rs
@@ -2,11 +2,12 @@ extern crate hamcrest2;
 
 #[cfg(test)]
 mod source_decoder_spec {
-    use log::info;
+    use log::{debug, info};
     use std::env;
     use crate::libs::source_codec::source_decoder::source_decode;
     use crate::libs::source_codec::source_encoding::Frame;
     use crate::libs::source_codec::test_encoding_builder::encoded;
+    use crate::libs::util::util::dump_byte_vec;
 
     #[ctor::ctor]
     fn before_each() {
@@ -19,43 +20,139 @@ mod source_decoder_spec {
 
     #[test]
     pub fn decode_emptiness() {
+        // Looks like Padding!
         let block = encoded(20, &[]);
-        let expected_frames = vec![];
+        let expected_frames = vec![Frame::Padding];
         assert_decoded_eq(block, expected_frames)
     }
 
     #[test]
     pub fn decode_complete_emptiness() {
-        should_decode_with_error(vec![])
+        should_decode_with_error(vec![], "Cannot decode a block of the wrong size")
     }
 
     #[test]
     pub fn decode_wrong_size() {
-        should_decode_with_error(vec![0, 0]);
+        should_decode_with_error(vec![0, 0], "Cannot decode a block of the wrong size");
     }
 
-    //#[test]
-    pub fn decode_all_types_of_frame() {
+    #[test]
+    pub fn first_padding_skips_other_stuff() {
+        let expected_frames = &[
+            Frame::Padding,
+        ];
+        // Can't use encoded() here since when you ask it to add a Padding, it fills the rest of
+        // the block with 0's, which we want to circumvent here.
+        let hand_coded_block = vec![0b00000101, 0, 0, 0, 0, 0, 0, 0];
+        assert_decoded_eq(hand_coded_block, expected_frames.to_vec());
+    }
+
+    #[test]
+    pub fn keying_without_wpmpolarity_cannot_be_decoded() {
+        let frames = vec![Frame::KeyingPerfectDit, Frame::KeyingPerfectDah, Frame::KeyingPerfectWordgap,
+                          Frame::KeyingDeltaDit { delta: 2 }, Frame::KeyingDeltaDah { delta: 2}, Frame::KeyingDeltaWordgap { delta: 4},
+            Frame::KeyingNaive { duration: 100 },
+            Frame::KeyingEnd
+        ];
+        // KeyingNaive doesn't need WPM, but it needs polarity to ensure the correct KeyingEvents are emitted.
+        for frame in frames {
+            let block = encoded(20, &[
+                frame,
+            ]);
+            should_decode_with_error(block, "Cannot decode keying without prior WPM|Polarity")
+        }
+    }
+
+    // Tests for specific frames, which all have to be surrounded by a WPMPolarity and Padding.
+
+    #[test]
+    pub fn decode_wpm_polarity() {
         let keying_frames = &[
             Frame::WPMPolarity { wpm: 20, polarity: true },
-            Frame::KeyingPerfectDit,
-            Frame::KeyingPerfectDah,
-            Frame::KeyingPerfectWordgap,
-            Frame::KeyingDeltaDit { delta: 5 },
-            Frame::KeyingDeltaDah { delta: -5 },
-            Frame::KeyingDeltaWordgap { delta: 5 },
+            Frame::Padding
         ];
         let block = encoded(20, keying_frames);
         assert_decoded_eq(block, keying_frames.to_vec());
     }
 
-    fn should_decode_with_error(block: Vec<u8>) {
+    #[test]
+    pub fn decode_perfect_dit() {
+        assert_decoded_frame(Frame::KeyingPerfectDit);
+    }
+
+    #[test]
+    pub fn decode_perfect_dah() {
+        assert_decoded_frame(Frame::KeyingPerfectDah);
+    }
+
+    #[test]
+    pub fn decode_perfect_wordgap() {
+        assert_decoded_frame(Frame::KeyingPerfectWordgap);
+    }
+
+    // Decode delta tests also prove that the WPMPolarity speed is given to the KeyingTiming, as if
+    // if it wasn't, it'd panic with WPM == 0.
+
+    #[test]
+    pub fn decode_delta_dit() {
+        assert_decoded_frame(Frame::KeyingDeltaDit { delta: -5 });
+        assert_decoded_frame(Frame::KeyingDeltaDit { delta: 5 });
+    }
+
+    #[test]
+    pub fn decode_delta_dah() {
+        assert_decoded_frame(Frame::KeyingDeltaDah { delta: -15 });
+        assert_decoded_frame(Frame::KeyingDeltaDah { delta: 15 });
+    }
+
+    #[test]
+    pub fn decode_delta_wordgap() {
+        assert_decoded_frame(Frame::KeyingDeltaWordgap { delta: -100 });
+        assert_decoded_frame(Frame::KeyingDeltaWordgap { delta: 100 });
+    }
+
+    #[test]
+    pub fn decode_naive() {
+        assert_decoded_frame(Frame::KeyingNaive { duration: 80 });
+    }
+
+    #[test]
+    pub fn decode_unused() {
+        assert_decoded_frame(Frame::Unused);
+    }
+
+    #[test]
+    pub fn decode_extension() {
+        assert_decoded_frame(Frame::Extension);
+    }
+
+    #[test]
+    pub fn wpm_polarity_causes_timing_recalculation() {
+        let keying_frames = &[
+            Frame::WPMPolarity { wpm: 5, polarity: true },
+            Frame::KeyingDeltaDah { delta: 5 },
+            Frame::WPMPolarity { wpm: 60, polarity: true },
+            Frame::KeyingDeltaDah { delta: 5 },
+            Frame::Extension, // It stands out as 1111 in the debug output below.
+            Frame::Padding
+        ];
+        let block = encoded(20, keying_frames);
+        debug!("{}", dump_byte_vec(&block));
+
+        // encoded tracks changes in speed when WPMPolarity frames are
+        // encoded, causing changes in length in the delta dah frames.
+        // They'll be correctly round-tripped here.
+        assert_decoded_eq(block, keying_frames.to_vec());
+    }
+
+    fn should_decode_with_error(block: Vec<u8>, expected_error_message: &str) {
         match source_decode(block) {
-            Ok(frames) => {
+            Ok(_) => {
                 panic!("Should not have successfully decoded")
             }
             Err(e) => {
                 info!("Expected error: {}", e);
+                assert_eq!(e.to_string(), expected_error_message);
             }
         }
     }
@@ -69,5 +166,15 @@ mod source_decoder_spec {
                 panic!("Should not fail with {}", e);
             }
         }
+    }
+
+    fn assert_decoded_frame(frame: Frame) {
+        let keying_frames = &[
+            Frame::WPMPolarity { wpm: 20, polarity: true },
+            frame,
+            Frame::Padding
+        ];
+        let block = encoded(20, keying_frames);
+        assert_decoded_eq(block, keying_frames.to_vec());
     }
 }
