85bada35c1251c6e7693756d699ee3d476afea5a
Matt Gumbley <matt.gumbley@devzendo.org>
2021-12-12 15:45:37 +0000

    Work in progress, using syncbox's ScheduledThreadPool.

--- src/libs/delayed_bus/delayed_bus.rs
+++ src/libs/delayed_bus/delayed_bus.rs
@@ -1,44 +1,35 @@
 use log::{debug, info};
-use std::cmp::Ordering;
-use std::collections::BinaryHeap;
 use std::fmt::Debug;
+use std::marker::PhantomData;
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::AtomicBool;
 use std::thread;
 use std::thread::JoinHandle;
 use std::time::Duration;
-use std::time::{SystemTime, UNIX_EPOCH};
 use bus::{Bus, BusReader};
+use syncbox::{ScheduledThreadPool, Task};
 
-#[readonly::make]
-pub struct DelayedBus<T> {
-    terminate: Arc<AtomicBool>,
+pub struct DelayedBus<T> where T: 'static + Send + Debug + Clone + Sync {
+    terminate_flag: Arc<AtomicBool>,
     read_thread_handle: Mutex<Option<JoinHandle<()>>>,
-    shared: Arc<Mutex<DelayedBusShared<T>>>,
+    spooky: PhantomData<T>,
 }
 
-impl<T: Send> DelayedBus<T> {
+impl<T: 'static + Send + Debug + Clone + Sync> DelayedBus<T> {
     pub fn new(input_rx: BusReader<T>, output_tx: Bus<T>, terminate: Arc<AtomicBool>, delay: Duration) -> Self {
         let arc_terminate = terminate.clone();
-        let mut heap = BinaryHeap::new();
-
-        let shared = Mutex::new(DelayedBusShared {
-            heap,
-            output_tx,
-        });
-        let arc_shared = Arc::new(shared);
-        let arc_shared_cloned = arc_shared.clone();
+        let arc_output_tx = Arc::new(Mutex::new(output_tx));
         let read_thread_handle = thread::spawn(move || {
             let mut read_thread = DelayedBusReadThread::new(delay, input_rx,
-                                                            arc_terminate,
-                                                            arc_shared.clone());
+                                                            arc_output_tx,
+                                                            arc_terminate);
             read_thread.thread_runner();
         });
 
         Self {
-            terminate,
+            terminate_flag: terminate,
             read_thread_handle: Mutex::new(Some(read_thread_handle)),
-            shared: arc_shared_cloned,
+            spooky: PhantomData,
         }
     }
 
@@ -47,7 +38,7 @@ impl<T: Send> DelayedBus<T> {
     // method other than this for blocking until it has actually stopped.
     pub fn terminate(&mut self) {
         debug!("Terminating delayed bus");
-        self.terminate.store(true, core::sync::atomic::Ordering::SeqCst);
+        self.terminate_flag.store(true, core::sync::atomic::Ordering::SeqCst);
         debug!("DelayedBus joining read thread handle...");
         let mut read_thread_handle = self.read_thread_handle.lock().unwrap();
         read_thread_handle.take().map(JoinHandle::join);
@@ -67,24 +58,10 @@ impl<T: Send> DelayedBus<T> {
     // }
 }
 
-impl<T> Drop for DelayedBus<T> {
+impl<T: Send + Debug + Clone + Sync + 'static> Drop for DelayedBus<T> {
     fn drop(&mut self) {
         debug!("DelayedBus signalling termination to thread on drop");
         self.terminate();
-    }
-}
-
-
-// An object shared between the main DelayedBus, and the DelayedBusThread.
-struct DelayedBusShared<T> {
-    output_tx: Bus<T>,
-    heap: BinaryHeap<TimedT<T>>,
-}
-
-impl<T: Debug> DelayedBusShared<T> {
-    fn emit(&mut self, item: T) {
-        debug!("Emitting {:?}", item);
-        self.output_tx.broadcast(item);
     }
 }
 
@@ -93,53 +70,26 @@ struct DelayedBusReadThread<T> {
     delay: Duration,
     terminate: Arc<AtomicBool>,
     input_rx: BusReader<T>,
-
-    // Shared state between thread and main code
-    shared: Arc<Mutex<DelayedBusShared<T>>>,
+    output_tx: Arc<Mutex<Bus<T>>>,
+    scheduled_thread_pool: ScheduledThreadPool,
 }
 
-#[derive(Copy, Clone)]
-struct TimedT<T> {
-    millis_since_epoch: u128,
-    item: T,
-}
 
-impl<T> Eq for TimedT<T> {}
-
-impl<T> PartialEq<Self> for TimedT<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.millis_since_epoch == other.millis_since_epoch
-    }
-}
-
-impl<T> PartialOrd<Self> for TimedT<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-// The priority queue depends on `Ord`.
-// Explicitly implement the trait so the queue becomes a min-heap
-// instead of a max-heap.
-impl<T> Ord for TimedT<T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        other.millis_since_epoch.cmp(&self.millis_since_epoch)
-    }
-}
-
-impl<T> DelayedBusReadThread<T> {
+impl<T: Send + Debug + Clone + Sync + 'static> DelayedBusReadThread<T> {
     fn new(delay: Duration,
            input_rx: BusReader<T>,
+           output_tx: Arc<Mutex<Bus<T>>>,
            terminate: Arc<AtomicBool>,
-           shared: Arc<Mutex<DelayedBusShared<T>>>
     ) -> Self {
         debug!("Constructing DelayedBusReadThread");
+        let scheduled_thread_pool = syncbox::ScheduledThreadPool::single_thread();
 
         Self {
             delay,
             terminate,
+            scheduled_thread_pool,
             input_rx,
-            shared,
+            output_tx,
         }
     }
 
@@ -154,10 +104,11 @@ impl<T> DelayedBusReadThread<T> {
 
             match self.input_rx.recv_timeout(Duration::from_millis(100)) {
                 Ok(item) => {
-                    let item_millis_since_epoch = get_epoch_ms();
-                    let item_later = self.delay.as_millis() + item_millis_since_epoch;
-                    let to_queue = TimedT::<T> { millis_since_epoch: item_later, item };
-                    self.shared.lock().unwrap().heap.push(to_queue);
+                    debug!("Received item {:?}", item);
+                    let item_later = self.delay.as_millis();
+                    let arc_output_tx = self.output_tx.clone();
+                    let task = TimedOutput{ item: item, output_tx: arc_output_tx };
+                    self.scheduled_thread_pool.schedule_ms(item_later as u32, task);
                 }
                 Err(_) => {
                     // Don't log, it's just noise - timeout gives opportunity to go round loop and
@@ -169,13 +120,18 @@ impl<T> DelayedBusReadThread<T> {
     }
 }
 
-fn get_epoch_ms() -> u128 {
-    SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_millis()
+struct TimedOutput<T> {
+    item: T,
+    output_tx: Arc<Mutex<Bus<T>>>,
 }
 
+impl<T: Debug + Send + Clone + Sync + 'static> Task for TimedOutput<T> {
+    fn run(mut self) {
+        debug!("Broadcasting item {:?}", self.item);
+        let mut output = self.output_tx.lock().unwrap();
+        output.broadcast(self.item);
+    }
+}
 
 #[cfg(test)]
 #[path = "./delayed_bus_spec.rs"]
