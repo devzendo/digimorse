46d1c2e45bc4832fdda661203a87cbebfd7099e1
Matt Gumbley <matt.gumbley@devzendo.org>
2021-12-14 11:21:28 +0000

    Fix warnings.

--- src/libs/source_codec/metadata_codec.rs
+++ src/libs/source_codec/metadata_codec.rs
@@ -1,25 +1,25 @@
 use crate::libs::source_codec::source_encoding::{Callsign, Locator, Power};
 
-pub fn encode_callsign(callsign: Callsign) -> u32 {
+pub fn encode_callsign(_callsign: Callsign) -> u32 {
     0 // TODO
 }
 
-pub fn decode_callsign(last_28_bits_of_encoded_callsign: u32) -> Callsign {
+pub fn decode_callsign(_last_28_bits_of_encoded_callsign: u32) -> Callsign {
     "M0CUV".to_string() as Callsign // TODO
 }
 
-pub fn encode_locator(locator: Locator) -> u16 {
+pub fn encode_locator(_locator: Locator) -> u16 {
     0
 }
 
-pub fn decode_locator(last_15_bits_of_encoded_locator: u16) -> Locator {
+pub fn decode_locator(_last_15_bits_of_encoded_locator: u16) -> Locator {
     "JO01".to_string() as Locator // TODO
 }
 
-pub fn encode_power(power: Power) -> u8 {
+pub fn encode_power(_power: Power) -> u8 {
     0
 }
 
-pub fn decode_power(last_n_bits_of_u8: u8) -> Power {
+pub fn decode_power(_last_n_bits_of_u8: u8) -> Power {
     0 as Power
 }
