360abafbf1d9c6eb130c5a03ea6e8334442b4a1f
Matt Gumbley <matt.gumbley@devzendo.org>
2022-03-08 18:22:19 +0000

    Convert test suite to use rstest.

--- src/libs/source_codec/source_decoder_spec.rs
+++ src/libs/source_codec/source_decoder_spec.rs
@@ -1,7 +1,5 @@
 extern crate hamcrest2;
 
-// TODO convert to rstest
-
 #[cfg(test)]
 mod source_decoder_spec {
     use log::{debug, info};
@@ -10,6 +8,7 @@ mod source_decoder_spec {
     use crate::libs::source_codec::source_encoding::Frame;
     use crate::libs::source_codec::test_encoding_builder::encoded;
     use crate::libs::util::util::dump_byte_vec;
+    use rstest::*;
 
     const TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS: usize = 64;
 
@@ -21,6 +20,18 @@ mod source_decoder_spec {
 
     #[ctor::dtor]
     fn after_each() {}
+
+    pub struct SourceDecoderFixture {
+        source_decoder: SourceDecoder,
+    }
+
+    #[fixture]
+    fn fixture() -> SourceDecoderFixture {
+        let source_decoder = SourceDecoder::new(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
+        SourceDecoderFixture {
+            source_decoder
+        }
+    }
 
     #[test]
     #[should_panic]
@@ -49,37 +60,37 @@ mod source_decoder_spec {
         let _ = SourceDecoder::new(block_size);
     }
 
-    #[test]
-    pub fn decode_emptiness() {
+    #[rstest]
+    pub fn decode_emptiness(fixture: SourceDecoderFixture) {
         // Looks like Padding!
         let block = encoded(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS, 20, &[]);
         let expected_frames = vec![Frame::Padding];
-        assert_decoded_eq(block, expected_frames)
+        assert_decoded_eq(&fixture, block, expected_frames)
     }
 
-    #[test]
-    pub fn decode_complete_emptiness() {
-        should_decode_with_error(vec![], "Cannot decode a block of the wrong size")
+    #[rstest]
+    pub fn decode_complete_emptiness(fixture: SourceDecoderFixture) {
+        should_decode_with_error(&fixture, vec![], "Cannot decode a block of the wrong size")
     }
 
-    #[test]
-    pub fn decode_wrong_size() {
-        should_decode_with_error(vec![0, 0], "Cannot decode a block of the wrong size");
+    #[rstest]
+    pub fn decode_wrong_size(fixture: SourceDecoderFixture) {
+        should_decode_with_error(&fixture, vec![0, 0], "Cannot decode a block of the wrong size");
     }
 
-    #[test]
-    pub fn first_padding_skips_other_stuff() {
+    #[rstest]
+    pub fn first_padding_skips_other_stuff(fixture: SourceDecoderFixture) {
         let expected_frames = &[
             Frame::Padding,
         ];
         // Can't use encoded() here since when you ask it to add a Padding, it fills the rest of
         // the block with 0's, which we want to circumvent here.
         let hand_coded_block = vec![0b00000101, 0, 0, 0, 0, 0, 0, 0];
-        assert_decoded_eq(hand_coded_block, expected_frames.to_vec());
+        assert_decoded_eq(&fixture, hand_coded_block, expected_frames.to_vec());
     }
 
-    #[test]
-    pub fn keying_without_wpmpolarity_cannot_be_decoded() {
+    #[rstest]
+    pub fn keying_without_wpmpolarity_cannot_be_decoded(fixture: SourceDecoderFixture) {
         let frames = vec![Frame::KeyingPerfectDit, Frame::KeyingPerfectDah, Frame::KeyingPerfectWordgap,
                           Frame::KeyingDeltaDit { delta: 2 }, Frame::KeyingDeltaDah { delta: 2}, Frame::KeyingDeltaWordgap { delta: 4},
             Frame::KeyingNaive { duration: 100 },
@@ -90,75 +101,74 @@ mod source_decoder_spec {
             let block = encoded(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS, 20, &[
                 frame,
             ]);
-            should_decode_with_error(block, "Cannot decode keying without prior WPM|Polarity")
+            should_decode_with_error(&fixture, block, "Cannot decode keying without prior WPM|Polarity")
         }
     }
 
     // Tests for specific frames, which all have to be surrounded by a WPMPolarity and Padding.
-
-    #[test]
-    pub fn decode_wpm_polarity() {
+    #[rstest]
+    pub fn decode_wpm_polarity(fixture: SourceDecoderFixture) {
         let keying_frames = &[
             Frame::WPMPolarity { wpm: 20, polarity: true },
             Frame::Padding
         ];
         let block = encoded(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS, 20, keying_frames);
-        assert_decoded_eq(block, keying_frames.to_vec());
+        assert_decoded_eq(&fixture, block, keying_frames.to_vec());
     }
 
-    #[test]
-    pub fn decode_perfect_dit() {
-        assert_decoded_frame(Frame::KeyingPerfectDit);
+    #[rstest]
+    pub fn decode_perfect_dit(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingPerfectDit);
     }
 
-    #[test]
-    pub fn decode_perfect_dah() {
-        assert_decoded_frame(Frame::KeyingPerfectDah);
+    #[rstest]
+    pub fn decode_perfect_dah(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingPerfectDah);
     }
 
-    #[test]
-    pub fn decode_perfect_wordgap() {
-        assert_decoded_frame(Frame::KeyingPerfectWordgap);
+    #[rstest]
+    pub fn decode_perfect_wordgap(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingPerfectWordgap);
     }
 
     // Decode delta tests also prove that the WPMPolarity speed is given to the KeyingTiming, as if
     // if it wasn't, it'd panic with WPM == 0.
 
-    #[test]
-    pub fn decode_delta_dit() {
-        assert_decoded_frame(Frame::KeyingDeltaDit { delta: -5 });
-        assert_decoded_frame(Frame::KeyingDeltaDit { delta: 5 });
+    #[rstest]
+    pub fn decode_delta_dit(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingDeltaDit { delta: -5 });
+        assert_decoded_frame(&fixture, Frame::KeyingDeltaDit { delta: 5 });
     }
 
-    #[test]
-    pub fn decode_delta_dah() {
-        assert_decoded_frame(Frame::KeyingDeltaDah { delta: -15 });
-        assert_decoded_frame(Frame::KeyingDeltaDah { delta: 15 });
+    #[rstest]
+    pub fn decode_delta_dah(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingDeltaDah { delta: -15 });
+        assert_decoded_frame(&fixture, Frame::KeyingDeltaDah { delta: 15 });
     }
 
-    #[test]
-    pub fn decode_delta_wordgap() {
-        assert_decoded_frame(Frame::KeyingDeltaWordgap { delta: -100 });
-        assert_decoded_frame(Frame::KeyingDeltaWordgap { delta: 100 });
+    #[rstest]
+    pub fn decode_delta_wordgap(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingDeltaWordgap { delta: -100 });
+        assert_decoded_frame(&fixture, Frame::KeyingDeltaWordgap { delta: 100 });
     }
 
-    #[test]
-    pub fn decode_naive() {
-        assert_decoded_frame(Frame::KeyingNaive { duration: 80 });
+    #[rstest]
+    pub fn decode_naive(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::KeyingNaive { duration: 80 });
     }
 
-    #[test]
-    pub fn decode_unused() {
-        assert_decoded_frame(Frame::Unused);
+    #[rstest]
+    pub fn decode_unused(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::Unused);
     }
 
-    #[test]
-    pub fn decode_extension() {
-        assert_decoded_frame(Frame::Extension);
+    #[rstest]
+    pub fn decode_extension(fixture: SourceDecoderFixture) {
+        assert_decoded_frame(&fixture, Frame::Extension);
     }
 
-    #[test]
-    pub fn wpm_polarity_causes_timing_recalculation() {
+    #[rstest]
+    pub fn wpm_polarity_causes_timing_recalculation(fixture: SourceDecoderFixture) {
         let keying_frames = &[
             Frame::WPMPolarity { wpm: 5, polarity: true },
             Frame::KeyingDeltaDah { delta: 5 },
@@ -173,12 +183,11 @@ mod source_decoder_spec {
         // encoded tracks changes in speed when WPMPolarity frames are
         // encoded, causing changes in length in the delta dah frames.
         // They'll be correctly round-tripped here.
-        assert_decoded_eq(block, keying_frames.to_vec());
+        assert_decoded_eq(&fixture, block, keying_frames.to_vec());
     }
 
-    fn should_decode_with_error(block: Vec<u8>, expected_error_message: &str) {
-        let source_decoder = SourceDecoder::new(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
-        match source_decoder.source_decode(block) {
+    fn should_decode_with_error(fixture: &SourceDecoderFixture, block: Vec<u8>, expected_error_message: &str) {
+        match fixture.source_decoder.source_decode(block) {
             Ok(_) => {
                 panic!("Should not have successfully decoded")
             }
@@ -189,9 +198,8 @@ mod source_decoder_spec {
         }
     }
 
-    fn assert_decoded_eq(block: Vec<u8>, expected_frames: Vec<Frame>) {
-        let source_decoder = SourceDecoder::new(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS);
-        match source_decoder.source_decode(block) {
+    fn assert_decoded_eq(fixture: &SourceDecoderFixture, block: Vec<u8>, expected_frames: Vec<Frame>) {
+        match fixture.source_decoder.source_decode(block) {
             Ok(frames) => {
                 assert_eq!(frames, expected_frames);
             }
@@ -201,13 +209,13 @@ mod source_decoder_spec {
         }
     }
 
-    fn assert_decoded_frame(frame: Frame) {
+    fn assert_decoded_frame(fixture: &SourceDecoderFixture, frame: Frame) {
         let keying_frames = &[
             Frame::WPMPolarity { wpm: 20, polarity: true },
             frame,
             Frame::Padding
         ];
         let block = encoded(TEST_SOURCE_ENCODER_BLOCK_SIZE_IN_BITS, 20, keying_frames);
-        assert_decoded_eq(block, keying_frames.to_vec());
+        assert_decoded_eq(fixture, block, keying_frames.to_vec());
     }
 }
