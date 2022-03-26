308626f2984dd88fa52580ad2d55377f3cb635d4
Matt Gumbley <matt.gumbley@devzendo.org>
2022-03-26 14:59:36 +0000

    Add the bus readers for keyer diag mode to the application.

--- src/libs/application/application.rs
+++ src/libs/application/application.rs
@@ -1,7 +1,7 @@
 use log::debug;
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::AtomicBool;
-use bus::Bus;
+use bus::{Bus, BusReader};
 use clap::arg_enum;
 use syncbox::ScheduledThreadPool;
 use crate::libs::keyer_io::keyer_io::KeyingEvent;
@@ -22,6 +22,11 @@ pub trait BusOutput<T> {
     fn set_output_tx(&mut self, output_tx: Arc<Mutex<Bus<T>>>);
 }
 
+pub trait BusInput<T> {
+    fn clear_input_tx(&mut self);
+    fn set_input_tx(&mut self, input_tx: Arc<Mutex<BusReader<T>>>);
+}
+
 // The Application handles all the wiring between the active components of the system. The wiring
 // 'loom' is different depending on the mode enum.
 // It also holds the termination flag, and system-wide concerns such as PortAudio, the scheduled
@@ -31,24 +36,113 @@ pub struct Application {
     scheduled_thread_pool: Arc<ScheduledThreadPool>,
     mode: Option<Mode>,
 
-    keying_event_bus: Option<Bus<KeyingEvent>>,
+    keyer: Option<Arc<Mutex<dyn BusOutput<KeyingEvent>>>>,
+    keying_event_bus: Option<Arc<Mutex<Bus<KeyingEvent>>>>,
+    tone_generator: Option<Arc<Mutex<dyn BusInput<KeyingEvent>>>>,
+    tone_generator_keying_event_rx: Option<Arc<Mutex<BusReader<KeyingEvent>>>>,
+    keyer_diag: Option<Arc<Mutex<dyn BusInput<KeyingEvent>>>>,
+    keyer_diag_keying_event_rx: Option<Arc<Mutex<BusReader<KeyingEvent>>>>,
+    source_encoder: Option<Arc<Mutex<dyn BusInput<KeyingEvent>>>>,
+    source_encoder_keying_event_rx: Option<Arc<Mutex<BusReader<KeyingEvent>>>>,
 }
 
 impl Application {
     pub fn set_mode(&mut self, mode: Mode) {
         self.mode = Some(mode);
         // TODO far more to do here, set up wiring for each Mode
+        // There's always a KeyingEvent bus, and a receiver of it for the tone generator.
+        self.keying_event_bus = Some(Arc::new(Mutex::new(Bus::new(16))));
+        self.tone_generator_keying_event_rx = Some(Arc::new(Mutex::new(self.keying_event_bus.as_ref().unwrap().lock().unwrap().add_rx())));
+
         match mode {
             Mode::KeyerDiag => {
-                self.keying_event_bus = Some(Bus::new(16));
+                self.keyer_diag_keying_event_rx = Some(Arc::new(Mutex::new(self.keying_event_bus.as_ref().unwrap().lock().unwrap().add_rx())));
             }
-            Mode::SourceEncoderDiag => {}
-            _ => {}
+            Mode::SourceEncoderDiag => {
+                self.source_encoder_keying_event_rx = Some(Arc::new(Mutex::new(self.keying_event_bus.as_ref().unwrap().lock().unwrap().add_rx())));
+            }
+            _ => {
+            }
         }
     }
 
     pub fn get_mode(&self) -> Option<Mode> {
         return self.mode.clone();
+    }
+
+    pub fn set_keyer(&mut self, mut keyer: Arc<Mutex<dyn BusOutput<KeyingEvent>>>) {
+        match &self.keying_event_bus {
+            None => {
+                panic!("Cannot set a keyer with no keying_event_bus");
+            }
+            Some(keying_event_bus) => {
+                self.keyer = Some(keyer.clone());
+                let bus = keying_event_bus.clone();
+                keyer.lock().as_mut().unwrap().set_output_tx(bus);
+            }
+        }
+    }
+
+    pub fn clear_keyer(&mut self) {
+        self.keyer = None;
+        // TODO unwire down
+    }
+
+
+    pub fn set_tone_generator(&mut self, mut tone_generator: Arc<Mutex<dyn BusInput<KeyingEvent>>>) {
+        match &self.tone_generator_keying_event_rx {
+            None => {
+                panic!("Cannot set a tone_generator with no tone_generator_keying_event_rx");
+            }
+            Some(keying_event_bus) => {
+                self.tone_generator = Some(tone_generator.clone());
+                let bus_reader = keying_event_bus.clone();
+                tone_generator.lock().as_mut().unwrap().set_input_tx(bus_reader);
+            }
+        }
+
+    }
+
+    pub fn clear_tone_generator(&mut self) {
+        // TODO unwire down
+    }
+
+
+    pub fn set_keyer_diag(&mut self, mut keyer_diag: Arc<Mutex<dyn BusInput<KeyingEvent>>>) {
+        match &self.keyer_diag_keying_event_rx {
+            None => {
+                panic!("Cannot set a keyer_diag with no keyer_diag_keying_event_rx");
+            }
+            Some(keying_event_bus) => {
+                self.keyer_diag = Some(keyer_diag.clone());
+                let bus_reader = keying_event_bus.clone();
+                keyer_diag.lock().as_mut().unwrap().set_input_tx(bus_reader);
+            }
+        }
+
+    }
+
+    pub fn clear_keyer_diag(&mut self) {
+        // TODO unwire down
+    }
+
+
+    pub fn set_source_encoder(&mut self, mut source_encoder: Arc<Mutex<dyn BusInput<KeyingEvent>>>) {
+        match &self.source_encoder_keying_event_rx {
+            None => {
+                panic!("Cannot set a source_encoder with no source_encoder_keying_event_rx");
+            }
+            Some(keying_event_bus) => {
+                self.source_encoder = Some(source_encoder.clone());
+                let bus_reader = keying_event_bus.clone();
+                source_encoder.lock().as_mut().unwrap().set_input_tx(bus_reader);
+            }
+        }
+
+    }
+
+    pub fn clear_source_encoder(&mut self) {
+        // TODO unwire down
     }
 }
 
@@ -63,7 +157,14 @@ impl Application {
             scheduled_thread_pool,
             mode: None,
 
+            keyer: None,
             keying_event_bus: None,
+            tone_generator: None,
+            tone_generator_keying_event_rx: None,
+            keyer_diag: None,
+            keyer_diag_keying_event_rx: None,
+            source_encoder: None,
+            source_encoder_keying_event_rx: None,
         }
     }
 
--- src/libs/application/application_spec.rs
+++ src/libs/application/application_spec.rs
@@ -3,16 +3,19 @@ extern crate hamcrest2;
 #[cfg(test)]
 mod application_spec {
     use std::env;
-    use std::sync::Arc;
+    use std::sync::{Arc, Mutex};
     use std::sync::atomic::{AtomicBool, Ordering};
+    use std::sync::mpsc::RecvTimeoutError;
+    use std::time::Duration;
 
     use bus::{Bus, BusReader};
     use hamcrest2::prelude::*;
-    use log::{debug, info};
+    use log::{debug, info, warn};
     use rstest::*;
     use hamcrest2::prelude::*;
     use syncbox::ScheduledThreadPool;
-    use crate::libs::application::application::{Application, Mode};
+    use crate::libs::application::application::{Application, BusInput, BusOutput, Mode};
+    use crate::libs::keyer_io::keyer_io::KeyingEvent;
 
     use crate::libs::util::test_util;
     use crate::libs::util::test_util::wait_n_ms;
@@ -79,6 +82,122 @@ mod application_spec {
     pub fn mode_keyer_diag(mut fixture: ApplicationFixture) {
         fixture.application.set_mode(Mode::KeyerDiag);
         assert_that!(fixture.application.get_mode(), has(Mode::KeyerDiag));
+    }
+
+    struct FakeKeyer {
+        keying: Vec<KeyingEvent>,
+        bus: Option<Arc<Mutex<Bus<KeyingEvent>>>>,
+    }
+    impl BusOutput<KeyingEvent> for FakeKeyer {
+        fn clear_output_tx(&mut self) {
+            self.bus = None;
+        }
+
+        fn set_output_tx(&mut self, output_tx: Arc<Mutex<Bus<KeyingEvent>>>) {
+            self.bus = Some(output_tx);
+        }
+    }
+    impl FakeKeyer {
+        fn new(keying: Vec<KeyingEvent>) -> Self {
+            Self {
+                keying,
+                bus: None
+            }
+        }
+        fn start(&mut self) {
+            match self.bus.clone() {
+                None => {
+                    warn!("No bus set in FakeKeyer");
+                }
+                Some(bus) => {
+                    info!("Sending keying from FakeKeyer");
+                    let mut guard = bus.lock().unwrap();
+                    for v in &self.keying {
+                        guard.broadcast(*v);
+                    }
+                    info!("Sent keying from FakeKeyer");
+                }
+            }
+        }
+    }
+
+    struct StubBusReader {
+        keying: Vec<KeyingEvent>,
+        bus_reader: Option<Arc<Mutex<BusReader<KeyingEvent>>>>,
+    }
+    impl BusInput<KeyingEvent> for StubBusReader {
+        fn clear_input_tx(&mut self) {
+            self.bus_reader = None;
+        }
+
+        fn set_input_tx(&mut self, input_tx: Arc<Mutex<BusReader<KeyingEvent>>>) {
+            self.bus_reader = Some(input_tx);
+        }
+    }
+    impl StubBusReader {
+        fn new() -> Self {
+            Self {
+                keying: vec![],
+                bus_reader: None
+            }
+        }
+
+        fn read(&mut self) -> Vec<KeyingEvent> {
+            match &self.bus_reader {
+                None => {
+                    panic!("No bus reader set in StubBusReader");
+                }
+                Some(bus_reader) => {
+                    loop {
+                        match bus_reader.clone().lock().unwrap().recv_timeout(Duration::from_millis(500)) {
+                            Ok(ke) => {
+                                self.keying.push(ke);
+                            }
+                            Err(_) => {
+                                info!("StubBusReader timed out on read");
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            info!("Out of StubBusReader read");
+            self.keying.clone()
+        }
+    }
+
+    #[rstest]
+    // No need for the FakeKeyer and StubBusReader to have their own threads, so long as no more
+    // than 16 elements are placed onto the bus.
+    pub fn keyer_diag_bus_wiring(mut fixture: ApplicationFixture) {
+        fixture.application.set_mode(Mode::KeyerDiag);
+        assert_that!(fixture.application.get_mode(), has(Mode::KeyerDiag));
+
+        let sent_keying = vec![KeyingEvent::Start(), KeyingEvent::End()];
+        let mut keyer = Arc::new(Mutex::new(FakeKeyer::new(sent_keying.clone())));
+        let application_keyer = keyer.clone();
+        fixture.application.set_keyer(application_keyer);
+
+        let tone_generator = Arc::new(Mutex::new(StubBusReader::new()));
+        let application_tone_generator = tone_generator.clone();
+        fixture.application.set_tone_generator(application_tone_generator);
+
+        let keyer_diag = Arc::new(Mutex::new(StubBusReader::new()));
+        let application_keyer_diag = keyer_diag.clone();
+        fixture.application.set_keyer_diag(application_keyer_diag);
+
+
+        keyer.lock().unwrap().start();
+        info!("Test sleeping");
+        test_util::wait_5_ms(); // give things time to start
+        info!("Test out of sleep");
+
+
+        let tone_generator_received_keying = tone_generator.lock().unwrap().read();
+        let keyer_diag_received_keying = keyer_diag.lock().unwrap().read();
+
+        assert_eq!(tone_generator_received_keying, sent_keying.clone());
+        assert_eq!(keyer_diag_received_keying, sent_keying.clone());
     }
 
 }
