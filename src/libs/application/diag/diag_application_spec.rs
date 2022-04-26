1a5b126e5c0c3bd37af38e4f2bfc9b18f2f1cb23
Matt Gumbley <matt.gumbley@devzendo.org>
2022-04-26 07:53:01 +0100

    Create KeyerDiag as a test within diag_application_spec.rs.

--- src/libs/application/diag/diag_application_spec.rs
+++ src/libs/application/diag/diag_application_spec.rs
@@ -6,20 +6,24 @@
 #[cfg(test)]
 mod diag_application_spec {
     use std::env;
+    use std::fs::File;
     use std::sync::{Arc, Mutex};
     use std::sync::atomic::{AtomicBool, Ordering};
+    use std::time::Duration;
+    use bus::BusReader;
+    use csv::Writer;
 
     use log::{debug, info};
     use portaudio as pa;
     use rstest::*;
     use syncbox::ScheduledThreadPool;
 
-    use crate::libs::application::application::{Application, Mode};
+    use crate::libs::application::application::{Application, BusInput, Mode};
     use crate::libs::audio::tone_generator::ToneGenerator;
     use crate::libs::config_dir::config_dir;
     use crate::libs::config_file::config_file::ConfigurationStore;
     use crate::libs::keyer_io::arduino_keyer_io::ArduinoKeyer;
-    use crate::libs::keyer_io::keyer_io::{Keyer, KeyerSpeed};
+    use crate::libs::keyer_io::keyer_io::{Keyer, KeyerSpeed, KeyingEvent};
     use crate::libs::serial_io::serial_io::DefaultSerialIO;
     use crate::libs::util::test_util;
 
@@ -132,6 +136,81 @@ mod diag_application_spec {
         }
     }
 
+    fn set_keyer_diag(_config: &mut ConfigurationStore, application: &mut Application) -> Arc<Mutex<KeyerDiag>> {
+        let keyer_diag = KeyerDiag::new("keying.csv", application.terminate_flag());
+        let application_keyer_diag = Arc::new(Mutex::new(keyer_diag));
+        let return_keyer_diag = application_keyer_diag.clone();
+        application.set_keyer_diag(application_keyer_diag);
+        return return_keyer_diag;
+    }
+
+    struct KeyerDiag {
+        bus_reader: Option<Arc<Mutex<BusReader<KeyingEvent>>>>,
+        csv_writer: Writer<File>,
+        terminate: Arc<AtomicBool>,
+    }
+
+    impl KeyerDiag {
+        fn new(csv_path: &str, terminate: Arc<AtomicBool>) -> Self {
+            let wtr = Writer::from_path(csv_path);
+            match wtr {
+                Ok(_) => {}
+                Err(err) => {
+                    panic!("Can't initialise CSV writer to path {}: {}", csv_path, err);
+                }
+            }
+            Self {
+                bus_reader: None,
+                csv_writer: wtr.unwrap(),
+                terminate
+            }
+        }
+
+        // Precondition: set_input_rx has been called.
+        fn process(&mut self) {
+            loop {
+                if self.terminate.load(Ordering::SeqCst) {
+                    break;
+                }
+                let result = self.bus_reader.as_deref().unwrap().lock().unwrap().recv_timeout(Duration::from_millis(250));
+                match result {
+                    Ok(keying_event) => {
+                        info!("KeyerDiag: Keying Event {}", keying_event);
+                        match keying_event {
+                            // KeyingTimedEvents give the duration at the END of a (mark|space). If the
+                            // key is now up, then we've just heard a mark (key down), and if it's now down,
+                            // we've just heard a space (key up).
+                            // If we see a start, that's just the starting key down edge of a mark; an
+                            // end is actually meaningless in terms of keying - it's just a timeout after
+                            // the user has ended keying. In terms of generating a histogram of
+                            // keying, the stream should be a single long over - ie no END/STARTs in the
+                            // middle - otherwise you'll see two consecutive MARKs, which makes no sense.
+                            KeyingEvent::Timed(timed) => {
+                                self.csv_writer.write_record(&[if timed.up { "MARK" } else { "SPACE" }, format!("{}", timed.duration).as_str()]).unwrap();
+                                self.csv_writer.flush().unwrap();
+                            }
+                            KeyingEvent::Start() => {}
+                            KeyingEvent::End() => {}
+                        }
+                    }
+                    Err(_) => {
+                        // be quiet, it's ok..
+                    }
+                }
+            }
+        }
+    }
+
+    impl BusInput<KeyingEvent> for KeyerDiag {
+        fn clear_input_rx(&mut self) {
+            self.bus_reader = None;
+        }
+
+        fn set_input_rx(&mut self, input_tx: Arc<Mutex<BusReader<KeyingEvent>>>) {
+            self.bus_reader = Some(input_tx);
+        }
+    }
+
     #[rstest]
     #[serial]
     #[ignore]
@@ -140,8 +219,9 @@ mod diag_application_spec {
         fixture.application.set_mode(Mode::KeyerDiag);
         set_keyer(&mut fixture.config, &mut fixture.application);
         set_tone_generator(&mut fixture.config, &mut fixture.application);
-
-        // TODO add the extracted keyer diag object.
+        let keyer_diag = set_keyer_diag(&mut fixture.config, &mut fixture.application);
+        debug!("processing keyer_diag");
+        keyer_diag.lock().unwrap().process();
         debug!("end mode_keyer_diag");
     }
 }
