d3d4cadebeb20f743173c943e6af6d80b6655cdd
Matt Gumbley <matt.gumbley@devzendo.org>
2022-12-11 11:51:43 +0000

    Skeleton of the transmitter. Does not generate any audio yet.

--- src/libs/transmitter/transmitter.rs
+++ src/libs/transmitter/transmitter.rs
@@ -1,24 +1,244 @@
-
+use std::error::Error;
+use std::f32::consts::PI;
+use std::sync::{Arc, Mutex, RwLock};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
+use std::thread::JoinHandle;
+use std::time::Duration;
+use bus::BusReader;
+use log::{debug, info, warn};
+use portaudio::{NonBlocking, Output, OutputStreamSettings, PortAudio, Stream};
+use portaudio as pa;
+use crate::libs::application::application::BusInput;
+use crate::libs::channel_codec::channel_encoding::ChannelEncoding;
 
 pub type RadioFrequencyMHz = u32;
 pub type AudioFrequencyKHz = u16;
 
 pub struct Transmitter {
-    _radio_frequency_mhz: RadioFrequencyMHz,
-    _audio_offset: AudioFrequencyKHz,
+    _radio_frequency_mhz: RadioFrequencyMHz, // TODO CAT controller will need this?
+    audio_offset: AudioFrequencyKHz,
+    sample_rate: u32,
+    dt: f32, // Reciprocal of the sample rate
+    terminate: Arc<AtomicBool>,
+    thread_handle: Option<JoinHandle<()>>,
+    stream: Option<Stream<NonBlocking, Output<f32>>>,
+    callback_data: Arc<RwLock<CallbackData>>,
+    silent: Arc<AtomicBool>,
+
+    // Shared between thread and Transmitter
+    input_rx: Arc<Mutex<Option<Arc<Mutex<BusReader<ChannelEncoding>>>>>>,
+}
+
+#[derive(Clone)]
+struct CallbackData {
+    amplitude: f32, // used for ramping up/down output waveform at start and end
+    audio_frequency: AudioFrequencyKHz,
+    delta_phase: f32, // added to the phase after recording each sample
+    phase: f32,       // sin(phase) is the sample value
 }
 
 impl Transmitter {
-    pub fn new() -> Self {
+    pub fn new(audio_offset: AudioFrequencyKHz, terminate: Arc<AtomicBool>,
+               /* TODO transmit halt AtomicBool */ /* TODO CAT controller passed in here */) -> Self {
+        // Share this holder between the Transmitter and its thread
+        let input_rx_holder: Arc<Mutex<Option<Arc<Mutex<BusReader<ChannelEncoding>>>>>> = Arc::new(Mutex::new(None));
+        let move_clone_input_rx_holder = input_rx_holder.clone();
+
+        info!("Initialising Transmitter");
+        let modulation_callback_data = CallbackData {
+            amplitude: 0.0,
+            audio_frequency: audio_offset,
+            delta_phase: 0.0,
+            phase: 0.0,
+        };
+        // TODO replace this Mutex with atomics to reduce contention in the callback.
+        let arc_lock_modulation_callback_data = Arc::new(RwLock::new(modulation_callback_data));
+        let move_clone_modulation_callback_data = arc_lock_modulation_callback_data.clone();
+
+        let silent = Arc::new(AtomicBool::new(true));
+
         Self {
             _radio_frequency_mhz: 0,
-            _audio_offset: 0
+            audio_offset,
+            sample_rate: 0, // will be initialised when the callback is initialised
+            dt: 0.0,        // will be initialised when the callback is initialised
+            terminate: terminate.clone(),
+            input_rx: input_rx_holder,    // Modified by BusInput
+            thread_handle: Some(thread::spawn(move || {
+                info!("Transmitter channel-encoding listener thread started");
+                loop {
+                    if terminate.load(Ordering::SeqCst) {
+                        info!("Terminating transmitter thread");
+                        break;
+                    }
+
+                    // Can be updated by the BusInput<ChannelEncoding> above
+                    let mut need_sleep = false;
+                    match move_clone_input_rx_holder.lock().unwrap().as_deref() {
+                        None => {
+                            // Input channel hasn't been set yet; sleep after releasing lock
+                            need_sleep = true;
+                        }
+                        Some(input_rx) => {
+                            match input_rx.lock().unwrap().recv_timeout(Duration::from_millis(50)) {
+                                Ok(channel_encoding) => {
+                                    info!("Transmitter got {:?}", channel_encoding);
+                                    let locked_callback_data = move_clone_modulation_callback_data.read().unwrap();
+                                    // TODO convert the channel_encoding into a GFSK waveform, and set it in the locked_callback_data
+                                    // for the callback to emit.
+                                    // TODO CAT transmit enable - or pass the CAT into the thread so it
+                                    // disables after it has modulated the channel_encoding.end ?
+
+                                }
+                                Err(_) => {
+                                    // could timeout, or be disconnected?
+                                    // ignore for now...
+                                }
+                            }
+                        }
+                    }
+                    if need_sleep {
+                        thread::sleep(Duration::from_millis(100));
+                    }
+                }
+                debug!("Transmitter channel-encoding listener thread stopped");
+            })),
+            callback_data: arc_lock_modulation_callback_data,
+            silent,
+            stream: None,
+        }
+    }
+
+    // The odd form of this callback setup (pass in the PortAudio and settings) rather than just
+    // returning the callback to the caller to do stuff with... is because I can't work out what
+    // the correct type signature of a callback-returning function should be.
+    pub fn start_callback(&mut self, pa: &PortAudio, mut output_settings: OutputStreamSettings<f32>) -> Result<(), Box<dyn Error>> {
+        let sample_rate = output_settings.sample_rate as u32;
+        self.sample_rate = sample_rate;
+        self.dt = 1.0_f32 / (sample_rate as f32);
+        debug!("sample rate is {}",sample_rate);
+        // self.set_delta_phase(0);
+
+        let move_clone_callback_data = self.callback_data.clone();
+        let callback = move |pa::OutputStreamCallbackArgs::<f32> { buffer, frames, .. }| {
+            // info!("buffer length is {}, frames is {}", buffer.len(), frames);
+            // buffer length is 128, frames is 64; idx goes from [0..128).
+            // One frame is a pair of left/right channel samples.
+            // 48000/64=750 so in one second there are 48000 samples (frames), and 750 calls to this callback.
+            // 1000/750=1.33333 so each buffer has a duration of 1.33333ms.
+            // The fastest dit we want to encode (at 60WPM) is 20ms long.
+
+            let mut idx = 0;
+
+            for _ in 0..frames {
+                // The processing of amplitude/phase/ramping needs to be done every frame.
+                let locked_callback_data = move_clone_callback_data.read().unwrap();
+/*                // TODO: Use a cosine ramping rather than this linear one?
+                match locked_callback_data.ramping {
+                    AmplitudeRamping::RampingUp => {
+                        if locked_callback_data.amplitude <= 0.0 {
+                            locked_callback_data.amplitude = 0.0;
+                            locked_callback_data.phase = 0.0;
+                        }
+                        if locked_callback_data.amplitude < 0.95 {
+                            locked_callback_data.amplitude += AMPLITUDE_DELTA;
+                        } else {
+                            locked_callback_data.amplitude = 0.95;
+                            locked_callback_data.ramping = AmplitudeRamping::Stable;
+                        }
+                    }
+                    AmplitudeRamping::RampingDown => {
+                        locked_callback_data.amplitude -= AMPLITUDE_DELTA;
+                        if locked_callback_data.amplitude <= 0.0 {
+                            locked_callback_data.amplitude = 0.0;
+                            locked_callback_data.ramping = AmplitudeRamping::Stable;
+                            locked_callback_data.phase = 0.0;
+                        }
+                    }
+                    AmplitudeRamping::Stable => {
+                        // noop
+                    }
+                }
+
+                locked_callback_data.phase += locked_callback_data.delta_phase;
+                let sine_val = f32::sin(locked_callback_data.phase) * locked_callback_data.amplitude;
+*/
+                let sine_val = 0.0_f32;
+                drop(locked_callback_data);
+
+                // TODO MONO - if opening the stream with a single channel causes the same values to
+                // be written to both left and right outputs, this could be optimised..
+                buffer[idx] = sine_val;
+                buffer[idx + 1] = sine_val;
+
+                idx += 2;
+            }
+            // idx is 128...
+            pa::Continue
+        };
+
+        // we won't output out of range samples so don't bother clipping them.
+        output_settings.flags = pa::stream_flags::CLIP_OFF;
+
+        let maybe_stream = pa.open_non_blocking_stream(output_settings, callback);
+        match maybe_stream {
+            Ok(mut stream) => {
+                stream.start()?;
+                self.stream = Some(stream);
+            }
+            Err(e) => {
+                warn!("Error opening tone generator output stream: {}", e);
+            }
+        }
+        Ok(())
+        // Now it's playing...
+    }
+
+    pub fn set_audio_frequency(&mut self, audio_frequency: AudioFrequencyKHz) -> () {
+        if self.sample_rate == 0 {
+            debug!("Sample rate not yet set; will set frequency when this is known");
+            return;
+        }
+        // TODO need to pass this across to the thread, where it'll cause the phase/etc to be updated.
+        {
+            let mut locked_callback_data = self.callback_data.write().unwrap();
+            locked_callback_data.audio_frequency = audio_frequency;
+            locked_callback_data.delta_phase = 2.0_f32 * PI * (locked_callback_data.audio_frequency as f32) / (self.sample_rate as f32);
+            debug!("Setting transmitter frequency to {}, sample_rate {}", locked_callback_data.audio_frequency, self.sample_rate);
+        }
+    }
+
+    pub fn is_silent(&self) -> bool {
+        self.silent.load(Ordering::SeqCst)
+    }
+}
+
+impl BusInput<ChannelEncoding> for Transmitter {
+    fn clear_input_rx(&mut self) {
+        match self.input_rx.lock() {
+            Ok(mut locked) => { *locked = None; }
+            Err(_) => {}
+        }
+    }
+
+    fn set_input_rx(&mut self, input_rx: Arc<Mutex<BusReader<ChannelEncoding>>>) {
+        match self.input_rx.lock() {
+            Ok(mut locked) => { *locked = Some(input_rx); }
+            Err(_) => {}
         }
     }
 }
 
-// Two implementations: an audio version for driving a traditional transmitter, and a direct version
-// that drives a DDS chip.
+impl Drop for Transmitter {
+    fn drop(&mut self) {
+        debug!("Transmitter stopping stream...");
+        self.stream.take().map(|mut r| r.stop());
+        debug!("Transmitter joining thread handle...");
+        self.thread_handle.take().map(JoinHandle::join);
+        debug!("Transmitter ...joined thread handle");
+    }
+}
 
 #[cfg(test)]
 #[path = "./transmitter_spec.rs"]
--- src/libs/transmitter/transmitter_spec.rs
+++ src/libs/transmitter/transmitter_spec.rs
@@ -1,15 +1,23 @@
 extern crate hamcrest2;
 
+// These are all manually run (and asserted correct aurally and with Audio Hijack / spectrum analyser).
 #[cfg(test)]
 mod transmitter_spec {
-    use std::env;
-    use std::sync::Arc;
-    use std::sync::atomic::{AtomicBool, Ordering};
-
+    use bus::Bus;
     use log::{debug, info};
+    use std::env;
     use rstest::*;
-
+    use std::sync::{Arc, Mutex};
+    use std::sync::atomic::{AtomicBool, Ordering};
+    use hamcrest2::prelude::*;
+    use crate::libs::audio::audio_devices::open_output_audio_device;
     use crate::libs::util::test_util;
+    use portaudio as pa;
+    use portaudio::PortAudio;
+    use crate::libs::application::application::BusInput;
+    use crate::libs::channel_codec::channel_encoding::ChannelEncoding;
+    use crate::libs::channel_codec::sample_channel_encoding::sample_channel_encoding;
+    use crate::libs::transmitter::transmitter::{AudioFrequencyKHz, Transmitter};
 
     #[ctor::ctor]
     fn before_each() {
@@ -22,19 +30,45 @@ mod transmitter_spec {
 
     pub struct TransmitterFixture {
         terminate: Arc<AtomicBool>,
+        channel_encoding_tx: Arc<Mutex<Bus<ChannelEncoding>>>,
+        transmitter: Transmitter,
+        pa: Arc<PortAudio>,
     }
 
     #[fixture]
     fn fixture() -> TransmitterFixture {
         let terminate = Arc::new(AtomicBool::new(false));
+        let mut channel_encoding_tx = Bus::new(16);
+        let channel_encoding_rx = channel_encoding_tx.add_rx();
+        let fixture_channel_encoding_tx = Arc::new(Mutex::new(channel_encoding_tx));
+
+        let old_macbook = true;
+        let dev = if old_macbook {"Built-in Output"} else {"MacBook Pro Speakers"};
+        let audio_frequency = 600 as AudioFrequencyKHz;
+        info!("Instantiating transmitter...");
+        let transmitter_channel_encoding_rx = Arc::new(Mutex::new(channel_encoding_rx));
+        let mut transmitter = Transmitter::new(audio_frequency,
+                                                 terminate.clone());
+        transmitter.set_input_rx(transmitter_channel_encoding_rx);
+
+        info!("Setting audio freqency...");
+        transmitter.set_audio_frequency(audio_frequency);
+
+        let mut fixture = TransmitterFixture {
+            terminate,
+            channel_encoding_tx: fixture_channel_encoding_tx,
+            transmitter,
+            pa: Arc::new(pa::PortAudio::new().unwrap()),
+        };
+        let output_settings = open_output_audio_device(&fixture.pa, dev).unwrap();
+        info!("Initialising audio callback...");
+        fixture.transmitter.start_callback(&fixture.pa, output_settings).unwrap();
+
         info!("Fixture setup sleeping");
-        test_util::wait_5_ms();
-        // give things time to start
+        test_util::wait_n_ms(100); // give things time to start
         info!("Fixture setup out of sleep");
 
-        TransmitterFixture {
-            terminate,
-        }
+        fixture
     }
 
     impl Drop for TransmitterFixture {
@@ -47,5 +81,31 @@ mod transmitter_spec {
     }
 
     #[rstest]
-    pub fn do_something(_fixture: TransmitterFixture) {}
+    #[serial]
+    #[ignore]
+    pub fn transmitter_is_silent_when_nothing_to_play(fixture: TransmitterFixture) {
+        assert_that!(fixture.transmitter.is_silent(), equal_to(true));
+        test_util::wait_5_ms();
+        assert_that!(fixture.transmitter.is_silent(), equal_to(true));
+        test_util::wait_5_ms();
+        assert_that!(fixture.transmitter.is_silent(), equal_to(true));
+        debug!("Done!");
+    }
+
+    #[rstest]
+    #[serial]
+    #[ignore]
+    pub fn play_gfsk_encoded_channel_encoding(fixture: TransmitterFixture) {
+        let channel_encoding = sample_channel_encoding();
+        debug!("Test sending channel encoding");
+        fixture.channel_encoding_tx.lock().unwrap().broadcast(channel_encoding);
+        debug!("Waiting for transmitter to send");
+        test_util::wait_5_ms();
+        assert_that!(fixture.transmitter.is_silent(), equal_to(true));
+        debug!("Waiting for transmitter to finish sending");
+        while !fixture.transmitter.is_silent() {
+            test_util::wait_5_ms();
+        }
+        debug!("Done!");
+    }
 }
