84d27ee466cf765ceaa29ace14cb427fc8298021
Matt Gumbley <matt.gumbley@devzendo.org>
2023-03-17 16:28:26 +0000

    Work in progress: Code speed field.

--- src/libs/gui/gui.rs
+++ src/libs/gui/gui.rs
@@ -13,33 +13,48 @@ use crate::libs::config_file::config_file::ConfigurationStore;
 use crate::libs::gui::message::Message;
 use crate::libs::util::version::VERSION;
 
-const WIDGET_WIDTH: i32 = 70;
 const WIDGET_HEIGHT: i32 = 25;
 const WIDGET_PADDING: i32 = 10;
 
 const WATERFALL_WIDTH: i32 = 350;
 const WATERFALL_HEIGHT: i32 = 250;
 
+// Central controls column
+const CENTRAL_CONTROLS_WIDTH: i32 = 120;
+
+const CODE_SPEED_WIDTH: i32 = 60;
+const CODE_SPEED_HEIGHT: i32 = 40;
+
+
 struct Gui {
     waterfall_canvas: Widget,
     status_output: Output,
+    code_speed_output: Output,
 }
+
 pub fn initialise(_config: &mut ConfigurationStore, _application: &mut Application) -> () {
     debug!("Initialising App");
     let app = App::default().with_scheme(Scheme::Gtk);
     debug!("Initialising Window");
     let mut wind = Window::default().with_label(format!("digimorse v{} de M0CUV", VERSION).as_str());
+
     // move this to the application
     let (sender, receiver) = channel::<Message>();
+
+    let waterfall_canvas_background = Color::from_hex_str("#aab0cb").unwrap();
+    let window_background = Color::from_hex_str("#dfe2ff").unwrap();
 
     let mut gui = Gui {
         waterfall_canvas: Widget::new(WIDGET_PADDING, WIDGET_PADDING, WATERFALL_WIDTH, WATERFALL_HEIGHT, ""),
         status_output: Output::default()
             .with_size(WATERFALL_WIDTH, WIDGET_HEIGHT)
             .with_pos(WIDGET_PADDING, WIDGET_PADDING + WATERFALL_HEIGHT + WIDGET_PADDING),
+        code_speed_output: Output::default()
+            .with_size(CODE_SPEED_WIDTH, CODE_SPEED_HEIGHT)
+            .with_pos(WIDGET_PADDING * 2 + WATERFALL_WIDTH, WIDGET_PADDING)
     };
+
     gui.waterfall_canvas.set_trigger(CallbackTrigger::Release);
-    let waterfall_canvas_background = Color::from_hex_str("#aab0cb").unwrap();
     gui.waterfall_canvas.draw(move |wid| {
         push_clip(wid.x(), wid.y(), wid.width(), wid.height());
         draw_rect_fill(wid.x(), wid.y(), wid.width(), wid.height(), waterfall_canvas_background);
@@ -48,14 +63,21 @@ pub fn initialise(_config: &mut ConfigurationStore, _application: &mut Applicati
         draw_rect(wid.x(), wid.y(), wid.width(), wid.height());
         pop_clip();
     });
+
     gui.status_output.set_color(Color::Black);
     gui.status_output.set_text_color(Color::from_hex_str("#f2cc91").unwrap());
     gui.status_output.set_value("status message");
+
+    gui.code_speed_output.set_color(window_background);
+    gui.code_speed_output.set_text_color(Color::Black);
+    gui.code_speed_output.set_value("16");
+    gui.code_speed_output.set_text_size(18);
+
     wind.set_size(
-        WATERFALL_WIDTH + 2*WIDGET_PADDING,
+        WIDGET_PADDING + WATERFALL_WIDTH + WIDGET_PADDING + CENTRAL_CONTROLS_WIDTH + WIDGET_PADDING,
         WATERFALL_HEIGHT + WIDGET_HEIGHT + 3*WIDGET_PADDING,
     );
-    wind.set_color(Color::from_hex_str("#dfe2ff").unwrap());
+    wind.set_color(window_background);
 
     wind.end();
     debug!("Showing main window");
