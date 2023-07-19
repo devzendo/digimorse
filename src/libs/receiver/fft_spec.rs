01d6ca6a966923874069cfe427f10683d10e0433
Matt Gumbley <matt.gumbley@devzendo.org>
2023-07-19 18:48:53 +0100

    WIP: FFT set up a sample input waveform.

--- src/libs/receiver/fft_spec.rs
+++ src/libs/receiver/fft_spec.rs
@@ -2,11 +2,11 @@
 mod fft_spec {
     use std::cell::RefCell;
     use std::env;
+    use std::f32::consts::PI;
     use std::sync::{Arc, Mutex};
-
-
+    use hamcrest2::prelude::*;
     use rstest::*;
-    use crate::libs::buffer_pool::observable_buffer::ObservableBufferSlice;
+    use crate::libs::buffer_pool::observable_buffer::{OBSERVABLE_BUFFER_SLICE_SIZE, ObservableBufferSlice};
     use crate::libs::patterns::observer::Observer;
     use crate::libs::receiver::fft::{FFTingBufferObserver, ObservableFrequencySlice};
 
@@ -57,8 +57,19 @@ mod fft_spec {
     #[rstest]
     #[serial]
     pub fn buffer_in_fft_out(fixture: Arc<FFTFixture>) {
-        let samples: Vec<f32> = vec![];
-
-
+        let mut samples: Vec<f32> = vec![];
+        // A 600Hz sine wave..
+        let freq: f32 = 600.0;
+        let sample_rate = 48000.0;
+        let delta_phase = 2.0_f32 * PI * freq / sample_rate;
+        let mut phase = 0.0;
+        for idx in 0..OBSERVABLE_BUFFER_SLICE_SIZE {
+            let sine_val = f32::sin(phase);
+            samples.push(sine_val);
+            phase += delta_phase;
+        }
+        let buffer = ObservableBufferSlice { slice: samples };
+        fixture.fft_observer.lock().unwrap().on_notify(&buffer);
+        assert_that!(fixture.fft_slices.borrow().len(), equal_to(1));
     }
 }
