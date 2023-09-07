bf7d54e70d0a9bea199f435e3724fa6d5b1fc385
Matt Gumbley <matt.gumbley@devzendo.org>
2023-09-07 07:40:14 +0100

    Process incoming data by transforming it with rustfft. Output data still needs to be normalised? The data emitted by the FFT is not what I expected - a single peak representing the 600Hz input frequency. Much more study of FFT required.

--- src/libs/receiver/fft.rs
+++ src/libs/receiver/fft.rs
@@ -1,23 +1,32 @@
 use std::sync::Arc;
-
+use log::debug;
+use rustfft::{Fft, FftPlanner};
+use rustfft::num_complex::Complex;
+// use rustfft::FFTplanner;
+// use rustfft::num_complex::Complex;
 use crate::libs::buffer_pool::observable_buffer::{OBSERVABLE_BUFFER_SLICE_SIZE, ObservableBufferSlice};
 use crate::libs::patterns::observer::{ConcreteObserverList, Observable, Observer, ObserverList};
 
 #[derive(Clone)]
 pub struct ObservableFrequencySlice {
-    // TODO output from FFT, whatever that is.
+    reals: Vec<f32>,
 }
 
 impl Observable for ObservableFrequencySlice {
 }
 
 pub struct FFTingBufferObserver {
+    fft: Arc<dyn Fft<f32>>,
     observers: ConcreteObserverList<ObservableFrequencySlice>,
 }
 
 impl FFTingBufferObserver {
     pub fn new() -> Self {
+       let mut planner = FftPlanner::<f32>::new();
+        let fft = planner.plan_fft_forward(OBSERVABLE_BUFFER_SLICE_SIZE * 2);
+
         Self {
+            fft,
             observers: ConcreteObserverList::new(),
         }
     }
@@ -36,8 +45,22 @@ impl Observer<ObservableBufferSlice<f32>> for FFTingBufferObserver {
         }
         // RefCell for interior mutability since the on_notify method does not have &mut self
         // self.observations.borrow_mut().push(observable.clone());
-        // TODO zero pad, fft, emit
-        let out = ObservableFrequencySlice {};
+
+        // Copy the real data from the one_sixty_ms_downsampled_audio into the first half of the buffer
+        // that rustfft uses as input/output - as complex data with a zero imaginary part. // TODO zero pad, fft, emit
+        let mut buffer = vec![Complex{ re: 0.0, im: 0.0 }; OBSERVABLE_BUFFER_SLICE_SIZE * 2];
+        for i in 0 .. OBSERVABLE_BUFFER_SLICE_SIZE {
+            buffer[i] = Complex{ re: one_sixty_ms_downsampled_audio.slice[i], im: 0.0};
+        }
+        debug!("calling FFT");
+        self.fft.process(&mut buffer);
+        debug!("called FFT");
+        let mut vec = Vec::with_capacity(OBSERVABLE_BUFFER_SLICE_SIZE * 2);
+        vec.resize(OBSERVABLE_BUFFER_SLICE_SIZE * 2, 0.0_f32);
+        for i in 0 .. OBSERVABLE_BUFFER_SLICE_SIZE * 2 {
+            vec[i] = buffer[i].im;
+        }
+        let out = ObservableFrequencySlice {reals: vec};
         self.observers.notify_observers(&out);
     }
 }
--- src/libs/receiver/fft_spec.rs
+++ src/libs/receiver/fft_spec.rs
@@ -1,14 +1,17 @@
 #[cfg(test)]
 mod fft_spec {
+    use crate::libs::buffer_pool::observable_buffer::{
+        ObservableBufferSlice, OBSERVABLE_BUFFER_SLICE_SIZE,
+    };
+    use crate::libs::patterns::observer::Observer;
+    use crate::libs::receiver::fft::{FFTingBufferObserver, ObservableFrequencySlice};
+    use hamcrest2::prelude::*;
+    use rstest::*;
     use std::cell::RefCell;
     use std::env;
     use std::f32::consts::PI;
     use std::sync::{Arc, Mutex};
-    use hamcrest2::prelude::*;
-    use rstest::*;
-    use crate::libs::buffer_pool::observable_buffer::{OBSERVABLE_BUFFER_SLICE_SIZE, ObservableBufferSlice};
-    use crate::libs::patterns::observer::Observer;
-    use crate::libs::receiver::fft::{FFTingBufferObserver, ObservableFrequencySlice};
+    use log::debug;
 
     #[ctor::ctor]
     fn before_each() {
@@ -33,17 +36,20 @@ mod fft_spec {
     #[fixture]
     fn fixture() -> Arc<FFTFixture> {
         let fft_observer = Arc::new(Mutex::new(FFTingBufferObserver::new()));
-        let mut fixture = Arc::new(FFTFixture {
+        let fixture = Arc::new(FFTFixture {
             fft_slices: RefCell::new(vec![]),
             fft_observer,
         });
-        fixture.fft_observer.lock().unwrap().add_observer(fixture.clone() as Arc<dyn Observer<ObservableFrequencySlice>>);
+        fixture
+            .fft_observer
+            .lock()
+            .unwrap()
+            .add_observer(fixture.clone() as Arc<dyn Observer<ObservableFrequencySlice>>);
         fixture.clone()
     }
 
     impl Drop for FFTFixture {
-        fn drop(&mut self) {
-        }
+        fn drop(&mut self) {}
     }
 
     #[rstest]
@@ -57,19 +63,26 @@ mod fft_spec {
     #[rstest]
     #[serial]
     pub fn buffer_in_fft_out(fixture: Arc<FFTFixture>) {
+        assert_that!(fixture.fft_slices.borrow().len(), equal_to(0));
+
         let mut samples: Vec<f32> = vec![];
         // A 600Hz sine wave..
         let freq: f32 = 600.0;
         let sample_rate = 48000.0;
         let delta_phase = 2.0_f32 * PI * freq / sample_rate;
         let mut phase = 0.0;
-        for idx in 0..OBSERVABLE_BUFFER_SLICE_SIZE {
+        for _idx in 0..OBSERVABLE_BUFFER_SLICE_SIZE {
             let sine_val = f32::sin(phase);
             samples.push(sine_val);
             phase += delta_phase;
         }
         let buffer = ObservableBufferSlice { slice: samples };
         fixture.fft_observer.lock().unwrap().on_notify(&buffer);
-        assert_that!(fixture.fft_slices.borrow().len(), equal_to(1));
+        let reals = fixture.fft_slices.borrow();
+        assert_that!(reals.len(), equal_to(1));
+        let fft = reals.get(0).unwrap();
+        for i in 0..OBSERVABLE_BUFFER_SLICE_SIZE * 2 {
+            println!("{}, {}", i, fft.reals[i]);
+        }
     }
 }
